01a9930b53a709dc5101cad4861c263f53618d41
Deanna Sharma <deanna.sharma@doza.consulting>
2024-02-27 12:11:09 +0000

    update query

--- src/functions/aggregate-siri/database.ts
+++ src/functions/aggregate-siri/database.ts
@@ -12,9 +12,12 @@ export const getCurrentAvlData = async (db: Kysely<Database>, logger: Logger) =>
 
     const avl = await db
         .selectFrom("avl")
+        .distinctOn("operatorRef" && "vehicleRef")
         .selectAll("avl")
-        .where(sql`"validUntilTime"::DATE`, ">=", sql<Date>`NOW()`)
+        .orderBy("operatorRef" && "vehicleRef", "desc")
         .execute()
 
     return avl
 }
+
+const data = [{"id":11,"responseTimeStamp":"2024-02-26T11:31:12+00:00","producerRef":"BODS-AVL","recordedAtTime":"2024-02-26T11:31:12+00:00","validUntilTime":"2024-02-28 11:36:26","lineRef":"25","directionRef":"outbound","operatorRef":"GOGO","datedVehicleJourneyRef":"1","vehicleRef":"XYZ-123","dataSource":"BODS","longitude":"-1.5492094","latitude":"53.7949385","bearing":"90","delay":"0","isCompleteStopSequence":"No","publishedLineName":"25","originRef":"The Origin","destinationRef":"The Destination","blockRef":null},{"id":3,"responseTimeStamp":"2024-02-26T11:31:12+00:00","producerRef":"BODS-AVL","recordedAtTime":"2024-02-26T11:31:12+00:00","validUntilTime":"2024-02-26 11:36:26","lineRef":"33","directionRef":"outbound","operatorRef":"FMAN","datedVehicleJourneyRef":"1","vehicleRef":"ABC","dataSource":"BODS","longitude":"-1.5613995","latitude":"53.7920412","bearing":"90","delay":"0","isCompleteStopSequence":"No","publishedLineName":"33","originRef":"Bus Station","destinationRef":"The Destination","blockRef":null}]
