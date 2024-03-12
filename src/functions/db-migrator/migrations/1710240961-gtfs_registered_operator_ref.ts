94452d4eb95d873e4401f20fbac34cb7527771f8
Nathan Betts <nathan.betts@doza.consulting>
2024-03-12 17:07:49 +0000

    fix property name in agency table

--- src/functions/db-migrator/migrations/1710240961-gtfs_registered_operator_ref.ts
+++ src/functions/db-migrator/migrations/1710240961-gtfs_registered_operator_ref.ts
@@ -2,9 +2,9 @@ import { Database } from "@bods-integrated-data/shared";
 import { Kysely } from "kysely";
 
 export async function up(db: Kysely<Database>): Promise<void> {
-    await db.schema.alterTable("agency").addColumn("registered_operator_ref", "text").execute();
+    await db.schema.alterTable("agency").addColumn("registeredOperatorRef", "text").execute();
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
-    await db.schema.alterTable("agency").dropColumn("registered_operator_ref").execute();
+    await db.schema.alterTable("agency").dropColumn("registeredOperatorRef").execute();
 }
