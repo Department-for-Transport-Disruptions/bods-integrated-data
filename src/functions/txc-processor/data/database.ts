e84fdf76db786fdc10f2fb22365cf5936d6a6dc3
tomkellykpmg <119508391+tomkellykpmg@users.noreply.github.com>
2024-03-13 17:26:25 +0000

    BODS-2969: Stops table (#30)
    * Added stops processing
    
    * Address review comments
    
    * Updated names to snake case

--- src/functions/txc-processor/data/database.ts
+++ src/functions/txc-processor/data/database.ts
@@ -1,5 +1,5 @@
-import { Database, notEmpty } from "@bods-integrated-data/shared";
-import { Operator } from "@bods-integrated-data/shared/schema";
+import { Database, LocationType, notEmpty } from "@bods-integrated-data/shared";
+import { Operator, Stop } from "@bods-integrated-data/shared/schema";
 import { Kysely } from "kysely";
 
 export const insertAgencies = async (dbClient: Kysely<Database>, operators: Operator[]) => {
@@ -27,4 +27,47 @@ export const insertAgencies = async (dbClient: Kysely<Database>, operators: Oper
     const agencyData = await Promise.all(agencyPromises);
 
     return agencyData.filter(notEmpty);
+};
+
+export const insertStops = async (dbClient: Kysely<Database>, stops: Stop[]) => {
+    const platformCodes = ["BCS", "PLT", "FBT"];
+    const stopsPromises = stops.map(async (stop) => {
+        const naptanStop = await dbClient
+            .selectFrom("naptan_stop")
+            .selectAll()
+            .where("atco_code", "=", stop.StopPointRef)
+            .executeTakeFirst();
+
+        const newStop = {
+            id: stop.StopPointRef,
+            wheelchair_boarding: 0,
+            parent_station: "",
+
+            ...(naptanStop
+                ? {
+                      stop_code: naptanStop.naptan_code,
+                      stop_name: naptanStop.common_name || stop.CommonName,
+                      stop_lat: naptanStop.latitude ? parseFloat(naptanStop.latitude) : stop.Location?.Latitude,
+                      stop_lon: naptanStop.longitude ? parseFloat(naptanStop.longitude) : stop.Location?.Longitude,
+                      location_type:
+                          naptanStop.stop_type === "RSE" ? LocationType.RealStationEntrance : LocationType.None,
+                      platform_code:
+                          naptanStop.stop_type && platformCodes.includes(naptanStop.stop_type)
+                              ? naptanStop.stop_type
+                              : "",
+                  }
+                : {
+                      stop_name: stop.CommonName,
+                      stop_lat: stop.Location?.Latitude,
+                      stop_lon: stop.Location?.Longitude,
+                      location_type: LocationType.None,
+                      platform_code: "",
+                  }),
+        };
+
+        return dbClient.insertInto("stop_new").values(newStop).returningAll().executeTakeFirst();
+    });
+
+    const stopData = await Promise.all(stopsPromises);
+    return stopData.filter(notEmpty);
 };
--- src/shared/schema/txc.schema.ts
+++ src/shared/schema/txc.schema.ts
@@ -5,12 +5,28 @@ export const operatorSchema = z.object({
     OperatorShortName: z.string(),
 });
 
+export const stopSchema = z.object({
+    StopPointRef: z.coerce.string(),
+    CommonName: z.string(),
+    Location: z.optional(
+        z.object({
+            Longitude: z.number(),
+            Latitude: z.number(),
+        }),
+    ),
+});
+
 export type Operator = z.infer<typeof operatorSchema>;
+
+export type Stop = z.infer<typeof stopSchema>;
 
 export const txcSchema = z.object({
     TransXChange: z.object({
         Operators: z.object({
             Operator: z.array(operatorSchema),
+        }),
+        StopPoints: z.object({
+            AnnotatedStopPointRef: z.array(stopSchema),
         }),
     }),
 });
