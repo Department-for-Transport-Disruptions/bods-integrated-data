29e60b5e98c90cbd12b64e43ebe2ab49860dfec8
Nathan Betts <nathan.betts@doza.consulting>
2024-03-28 09:47:10 +0000

    add tests for mapTimingLinkToStopTime

--- src/functions/txc-processor/utils.test.ts
+++ src/functions/txc-processor/utils.test.ts
@@ -1,7 +1,220 @@
-import { describe, expect, it } from "vitest";
-import { getFirstNonZeroDuration } from "./utils";
+import { logger } from "@baselime/lambda-logger";
+import { DropOffType, NewStopTime, PickupType, Timepoint } from "@bods-integrated-data/shared/database";
+import { getDate } from "@bods-integrated-data/shared/dates";
+import { AbstractTimingLink } from "@bods-integrated-data/shared/schema";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { getFirstNonZeroDuration, mapTimingLinkToStopTime } from "./utils";
 
 describe("utils", () => {
+    vi.mock("@baselime/lambda-logger", () => ({
+        logger: {
+            warn: vi.fn(),
+        },
+    }));
+
+    afterEach(() => {
+        vi.resetAllMocks();
+    });
+
+    describe("mapTimingLinkToStopTime", () => {
+        it("returns an empty object when no stop point ref is found", () => {
+            const currentDepartureTime = getDate("00:00:00");
+            const journeyPatternTimingLink: AbstractTimingLink = {
+                "@_id": "1",
+            };
+
+            const result = mapTimingLinkToStopTime(
+                "from",
+                currentDepartureTime,
+                "trip_id",
+                0,
+                journeyPatternTimingLink,
+            );
+
+            expect(result).toEqual({});
+            expect(logger.warn).toHaveBeenCalledWith(
+                `Missing stop point ref for journey pattern timing link with ref: ${journeyPatternTimingLink["@_id"]}`,
+            );
+        });
+
+        it("returns a stop time using journey pattern timing link 'From' data when it is defined", () => {
+            const currentDepartureTime = getDate("01/01/2024 00:00:00");
+            const journeyPatternTimingLink: AbstractTimingLink = {
+                RunTime: "PT1M",
+                From: {
+                    StopPointRef: "stop_id",
+                    Activity: "pickUpAndSetDown",
+                    TimingStatus: "principalTimingPoint",
+                },
+            };
+
+            const stopTime: NewStopTime = {
+                trip_id: "trip_id",
+                stop_id: "stop_id",
+                arrival_time: "00:00:00",
+                departure_time: "00:00:00",
+                stop_sequence: 0,
+                stop_headsign: "",
+                pickup_type: PickupType.Pickup,
+                drop_off_type: DropOffType.DropOff,
+                shape_dist_traveled: 0,
+                timepoint: Timepoint.Exact,
+            };
+
+            const result = mapTimingLinkToStopTime(
+                "from",
+                currentDepartureTime,
+                "trip_id",
+                0,
+                journeyPatternTimingLink,
+            );
+
+            expect(result.runTime?.asMinutes()).toEqual(1);
+            expect(result.stopTime).toEqual(stopTime);
+        });
+
+        it("returns a stop time using vehicle journey timing link 'From' data when journey pattern timing link data is not defined", () => {
+            const currentDepartureTime = getDate("01/01/2024 00:00:00");
+            const vehicleJourneyTimingLink: AbstractTimingLink = {
+                RunTime: "PT1M",
+                To: {
+                    StopPointRef: "stop_id",
+                    Activity: "pickUpAndSetDown",
+                    TimingStatus: "principalTimingPoint",
+                },
+            };
+
+            const stopTime: NewStopTime = {
+                trip_id: "trip_id",
+                stop_id: "stop_id",
+                arrival_time: "00:00:00",
+                departure_time: "00:00:00",
+                stop_sequence: 0,
+                stop_headsign: "",
+                pickup_type: PickupType.Pickup,
+                drop_off_type: DropOffType.DropOff,
+                shape_dist_traveled: 0,
+                timepoint: Timepoint.Exact,
+            };
+
+            const result = mapTimingLinkToStopTime(
+                "to",
+                currentDepartureTime,
+                "trip_id",
+                0,
+                {},
+                vehicleJourneyTimingLink,
+            );
+
+            expect(result.runTime?.asMinutes()).toEqual(1);
+            expect(result.stopTime).toEqual(stopTime);
+        });
+
+        it("returns a stop time using journey pattern timing link 'To' data when it is defined", () => {
+            const currentDepartureTime = getDate("01/01/2024 00:00:00");
+            const journeyPatternTimingLink: AbstractTimingLink = {
+                RunTime: "PT1M",
+                To: {
+                    StopPointRef: "stop_id",
+                    Activity: "pickUpAndSetDown",
+                    TimingStatus: "principalTimingPoint",
+                },
+            };
+
+            const stopTime: NewStopTime = {
+                trip_id: "trip_id",
+                stop_id: "stop_id",
+                arrival_time: "00:00:00",
+                departure_time: "00:00:00",
+                stop_sequence: 0,
+                stop_headsign: "",
+                pickup_type: PickupType.Pickup,
+                drop_off_type: DropOffType.DropOff,
+                shape_dist_traveled: 0,
+                timepoint: Timepoint.Exact,
+            };
+
+            const result = mapTimingLinkToStopTime("to", currentDepartureTime, "trip_id", 0, journeyPatternTimingLink);
+
+            expect(result.runTime?.asMinutes()).toEqual(1);
+            expect(result.stopTime).toEqual(stopTime);
+        });
+
+        it("returns a stop time using vehicle journey timing link 'To' data when journey pattern timing link data is not defined", () => {
+            const currentDepartureTime = getDate("01/01/2024 00:00:00");
+            const vehicleJourneyTimingLink: AbstractTimingLink = {
+                RunTime: "PT1M",
+                From: {
+                    StopPointRef: "stop_id",
+                    Activity: "pickUpAndSetDown",
+                    TimingStatus: "principalTimingPoint",
+                },
+            };
+
+            const stopTime: NewStopTime = {
+                trip_id: "trip_id",
+                stop_id: "stop_id",
+                arrival_time: "00:00:00",
+                departure_time: "00:00:00",
+                stop_sequence: 0,
+                stop_headsign: "",
+                pickup_type: PickupType.Pickup,
+                drop_off_type: DropOffType.DropOff,
+                shape_dist_traveled: 0,
+                timepoint: Timepoint.Exact,
+            };
+
+            const result = mapTimingLinkToStopTime(
+                "from",
+                currentDepartureTime,
+                "trip_id",
+                0,
+                {},
+                vehicleJourneyTimingLink,
+            );
+
+            expect(result.runTime?.asMinutes()).toEqual(1);
+            expect(result.stopTime).toEqual(stopTime);
+        });
+
+        it("returns a stop time with a different departure time when wait time data is defined", () => {
+            const currentDepartureTime = getDate("01/01/2024 00:00:00");
+            const journeyPatternTimingLink: AbstractTimingLink = {
+                RunTime: "PT1M",
+                From: {
+                    StopPointRef: "stop_id",
+                    Activity: "pickUpAndSetDown",
+                    TimingStatus: "principalTimingPoint",
+                    WaitTime: "PT30S",
+                },
+            };
+
+            const stopTime: NewStopTime = {
+                trip_id: "trip_id",
+                stop_id: "stop_id",
+                arrival_time: "00:00:00",
+                departure_time: "00:00:30",
+                stop_sequence: 0,
+                stop_headsign: "",
+                pickup_type: PickupType.Pickup,
+                drop_off_type: DropOffType.DropOff,
+                shape_dist_traveled: 0,
+                timepoint: Timepoint.Exact,
+            };
+
+            const result = mapTimingLinkToStopTime(
+                "from",
+                currentDepartureTime,
+                "trip_id",
+                0,
+                journeyPatternTimingLink,
+            );
+
+            expect(result.runTime?.asMinutes()).toEqual(1);
+            expect(result.stopTime).toEqual(stopTime);
+        });
+    });
+
     describe("getFirstNonZeroDuration", () => {
         it("returns undefined when there are no durations", () => {
             const result = getFirstNonZeroDuration([]);
