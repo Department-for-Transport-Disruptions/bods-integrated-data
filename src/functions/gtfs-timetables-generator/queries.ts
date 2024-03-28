1a7826049061b319110005f1e0c56292bf7d523f
Laurence Jones <laurence.jones@doza.consulting>
2024-03-28 16:22:49 +0000

    feat: include stop_times in gtfs

--- src/functions/gtfs-timetables-generator/queries.ts
+++ src/functions/gtfs-timetables-generator/queries.ts
@@ -133,4 +133,22 @@ export const queryBuilder = (dbClient: Kysely<Database>): Query[] => [
             .compile().sql,
         fileName: "feed_info",
     },
+    {
+        query: dbClient
+            .selectFrom("stop_time_new")
+            .select([
+                "trip_id",
+                "arrival_time",
+                "departure_time",
+                "stop_id",
+                "stop_sequence",
+                "stop_headsign",
+                "pickup_type",
+                "drop_off_type",
+                "shape_dist_traveled",
+                "timepoint",
+            ])
+            .compile().sql,
+        fileName: "stop_times",
+    },
 ];
