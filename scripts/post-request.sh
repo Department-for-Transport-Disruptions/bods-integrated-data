76b6505be2dde368e371a415daaeee679b58ddb4
mohit-kpmg <mohit.vashishat@kpmg.co.uk>
2024-04-03 12:56:17 +0100

    Script code updated

--- scripts/post-request.sh
+++ scripts/post-request.sh
@@ -9,7 +9,7 @@ helpFunction()
    echo ""
    echo "Usage: $0 -a APIGatewayEndpoint"
    echo -e "\t-a Pass the environment name"
-   exit 1 # Exit script after printing help
+   exit 1 # Exit script after printing helpgit 
 }
 
 while getopts "a:" opt
@@ -26,14 +26,16 @@ then
    helpFunction
 fi
 
-count=10
-uuid_var=$(uuidgen | tr 'A-Z' 'a-z')
+count=100
+date_file=$(date +"%Y-%m-%d")
 for i in $(seq $count)
 do
-  echo "starting ssh database tunnel"
-  sed  "s/{CURRENT_TIMESTAMP}/$(date +'%Y-%m-%d %H:%M:%S')/g" template.xml > temp.xml
-  sed  "s/{CURRENT_DATE}/$(date +'%Y-%m-%d')/g" temp.xml > temp1.xml
-  sed  "s/{RANDOM_UUID}/$(uuid_var)/g" temp1.xml > temp2.xml
-  curl -X POST -H 'Content-Type: application/xml' -d @temp2.xml $APIGatewayEndpoint
+  echo "Starting Data Endpoint Post request"
+  uuid_var=$(uuidgen | tr 'A-Z' 'a-z')
+  echo "Cureent Loop number is : $i and Item identifier is : $uuid_var" >> logfile_$date_file.txt
+  sed -e  "s/{CURRENT_TIMESTAMP}/$(date +'%Y-%m-%d %H:%M:%S')/g" -e "s/{CURRENT_DATE}/$(date +'%Y-%m-%d')/g" -e "s/{RANDOM_UUID}/$uuid_var/g" -e "s/{CURRENT_TIMESTAMP_PLUS_6_HOURS}/$(date -v+6H +'%Y-%m-%d %H:%M:%S')/g"  template.xml > temp.xml
+  curl -X POST -H 'Content-Type: application/xml' -d @temp.xml $APIGatewayEndpoint
+  sleep 5
 done
+rm temp.xml
 
