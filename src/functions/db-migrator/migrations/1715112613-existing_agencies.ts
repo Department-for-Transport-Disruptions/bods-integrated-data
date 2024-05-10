a18fa5bfe6d4964b00f2093dad534542dd17db65
Laurence Jones <laurence.jones@doza.consulting>
2024-05-10 12:05:12 +0100

    fix: set agency sequence after inserting existing agencies

--- src/functions/db-migrator/migrations/1715112613-existing_agencies.ts
+++ src/functions/db-migrator/migrations/1715112613-existing_agencies.ts
@@ -1,10 +1,12 @@
 import agencies from "@bods-integrated-data/shared/data/agencies.json";
 import { Database } from "@bods-integrated-data/shared/database";
-import { Kysely } from "kysely";
+import { Kysely, sql } from "kysely";
 
 export async function up(db: Kysely<Database>): Promise<void> {
     await db.deleteFrom("agency").execute();
     await db.insertInto("agency").values(agencies).execute();
+
+    await sql`SELECT setval(pg_get_serial_sequence('agency', 'id'), (select max(id) from agency))`.execute(db);
 }
 
 export async function down(): Promise<void> {}
