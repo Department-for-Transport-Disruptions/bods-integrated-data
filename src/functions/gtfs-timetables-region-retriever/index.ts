7af01a78fb429322f560268c812db10bcd4b4f25
Deanna Sharma <deanna.sharma@doza.consulting>
2024-05-13 09:44:28 +0100

    wip

--- src/functions/gtfs-timetables-region-retriever/index.ts
+++ src/functions/gtfs-timetables-region-retriever/index.ts
@@ -2,6 +2,12 @@ import { logger } from "@baselime/lambda-logger";
 import { listS3Objects } from "@bods-integrated-data/shared/s3";
 import { notEmpty } from "@bods-integrated-data/shared/utils";
 
+const regionMappings: { [key: string]: string } = {
+    L: "London",
+    ALL: "All",
+    EA: "East Anglia",
+};
+
 export const handler = async () => {
     try {
         const { BUCKET_NAME: bucketName } = process.env;
@@ -14,13 +20,20 @@ export const handler = async () => {
             Bucket: bucketName,
         });
 
-        if (!objects) {
+        if (!objects || !objects.Contents) {
             logger.warn("No files found in bucket.");
+            return [];
         }
 
-        const fileNames = objects.Contents?.map((item) => item.Key).filter(notEmpty);
+        const regionFileNames = objects.Contents?.map((item) => item.Key?.slice(0, -9).toUpperCase()).filter(notEmpty);
 
-        logger.info("data", fileNames as Object);
+        const res = regionFileNames.map((region) => ({
+            regionCode: region,
+            regionName: regionMappings[region],
+        }));
+
+        logger.info("data", res);
+        return res;
     } catch (e) {
         if (e instanceof Error) {
             logger.error("There was an error when retrieving GTFS regions.", e);
