0a7e5654f9e2d4b745d7798832262b1c3939ec70
Tom Kelly <thomas.kelly1@kpmg.co.uk>
2024-05-15 09:35:55 +0100

    Add local check

--- src/functions/db-migrator/migrations/1715610061-add_avl_geom.ts
+++ src/functions/db-migrator/migrations/1715610061-add_avl_geom.ts
@@ -2,7 +2,9 @@ import { Database } from "@bods-integrated-data/shared/database";
 import { Kysely, sql } from "kysely";
 
 export async function up(db: Kysely<Database>): Promise<void> {
-    await sql`CREATE EXTENSION IF NOT EXISTS postgis CASCADE`.execute(db);
+    if (process.env.STAGE !== "local") {
+        await sql`CREATE EXTENSION IF NOT EXISTS postgis CASCADE`.execute(db);
+    }
     await db.schema
         .alterTable("avl")
         .addColumn("geom", sql`geometry(POINT, 4326)`)
@@ -13,7 +15,9 @@ export async function up(db: Kysely<Database>): Promise<void> {
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
-    await sql`DROP EXTENSION IF EXISTS postgis CASCADE`.execute(db);
+    if (process.env.STAGE !== "local") {
+        await sql`DROP EXTENSION IF EXISTS postgis CASCADE`.execute(db);
+    }
     await db.schema.alterTable("avl").dropColumn("geom").execute();
     await db.schema.dropIndex("idx_avl_geom").on("avl").execute();
 }
