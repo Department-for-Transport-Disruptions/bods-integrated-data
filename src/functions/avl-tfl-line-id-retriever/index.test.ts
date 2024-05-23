15457d76a1a073d8bd91a8fabae1fa35ea93b408
mohit-kpmg <mohit.vashishat@kpmg.co.uk>
2024-05-23 14:09:07 +0100

    fixed comments

--- src/functions/avl-tfl-line-id-retriever/index.test.ts
+++ src/functions/avl-tfl-line-id-retriever/index.test.ts
@@ -1,11 +1,7 @@
 import { describe, it } from "vitest";
 
 describe("getLineIds", () => {
-    it("should return an array of line IDs if API returns data", () => {
-
-    });
-    it("should throw an error if an empty response is returned from API", () => {
-    });
-    it("should throw an error if an error is return from the API", () => {
-    });
+    it("should return an array of line IDs if API returns data", () => {});
+    it("should throw an error if an empty response is returned from API", () => {});
+    it("should throw an error if an error is return from the API", () => {});
 });
