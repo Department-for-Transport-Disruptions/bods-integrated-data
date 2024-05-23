bfde2b6510a35c44f4a396322c904960711b1e19
mohit-kpmg <mohit.vashishat@kpmg.co.uk>
2024-05-23 14:07:09 +0100

    fixed comments

--- src/functions/avl-tfl-line-id-retriever/index.ts
+++ src/functions/avl-tfl-line-id-retriever/index.ts
@@ -1,6 +1,5 @@
 import { logger } from "@baselime/lambda-logger";
 import { KyselyDb, NewTflLine, getDatabaseClient } from "@bods-integrated-data/shared/database";
-import { chunkArray } from "@bods-integrated-data/shared/utils";
 import axios from "axios";
 import { TflLinesSchema } from "./tfl-line.schema";
 
