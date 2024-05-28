23e20a6dff19d0e0189f7eed686827d9c2f3b3d4
Deanna Sharma <deanna.sharma@doza.consulting>
2024-05-28 16:03:00 +0100

    lint

--- src/shared/schema/txc.schema.ts
+++ src/shared/schema/txc.schema.ts
@@ -188,7 +188,9 @@ export const journeyPatternSectionSchema = z.object({
 export type TxcJourneyPatternSection = z.infer<typeof journeyPatternSectionSchema>;
 
 export const vehicleTypeSchema = z.object({
-    WheelchairAccessible: z.string().optional().transform((value) => value === "true" ? true : value === "false" ? false : undefined),
+    WheelchairAccessible: z.string()
+        .optional()
+        .transform((value) => value === "true" ? true : value === "false" ? false : undefined),
     VehicleEquipment: z
         .object({
             WheelchairEquipment: z
