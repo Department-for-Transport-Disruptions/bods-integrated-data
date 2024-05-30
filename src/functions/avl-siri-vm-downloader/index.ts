fa8c0beb50fa42b5c6f19c0c5712b562c72000f5
Deanna Sharma <deanna.sharma@doza.consulting>
2024-05-30 09:10:30 +0100

    comment out gtfs_rt module in local env

--- src/functions/avl-siri-vm-downloader/index.ts
+++ src/functions/avl-siri-vm-downloader/index.ts
@@ -1,6 +1,6 @@
 import { logger } from "@baselime/lambda-logger";
 import { AGGREGATED_SIRI_VM_FILE_PATH } from "@bods-integrated-data/shared/avl/utils";
-import { getS3Object } from "@bods-integrated-data/shared/s3";
+import { getPresignedUrl, getS3Object } from "@bods-integrated-data/shared/s3";
 
 export const handler = async () => {
     const { BUCKET_NAME: bucketName } = process.env;
@@ -16,18 +16,13 @@ export const handler = async () => {
     }
 
     try {
-        const data = await getS3Object({ Bucket: bucketName, Key: key });
-
-        if (!data.Body) {
-            throw new Error(`Unable to retrieve SIRI-VM data with key: ${key}`);
-        }
-
-        const body = await data.Body.transformToString();
+        const presignedUrl = await getPresignedUrl({ Bucket: bucketName, Key: key }, 3600);
 
         return {
-            statusCode: 200,
-            headers: { "Content-Type": "application/xml" },
-            body: body,
+            statusCode: 302,
+            headers: {
+                Location: presignedUrl,
+            },
         };
     } catch (error) {
         if (error instanceof Error) {
