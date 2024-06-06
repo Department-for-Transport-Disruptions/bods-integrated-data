bebdf5e7bccb625d5e8e93f9dc91dedce9fcd023
Nathan Betts <nathan.betts@doza.consulting>
2024-06-06 11:01:11 +0100

    fix column and index names

--- src/functions/db-migrator/migrations/1717691227-avl_query_indexes.ts
+++ src/functions/db-migrator/migrations/1717691227-avl_query_indexes.ts
@@ -2,21 +2,21 @@ import { Database } from "@bods-integrated-data/shared/database";
 import { Kysely } from "kysely";
 
 export async function up(db: Kysely<Database>): Promise<void> {
-    await db.schema.createIndex("idx_avl_operatorRef").on("avl").column("operatorRef").execute();
-    await db.schema.createIndex("idx_avl_vehicleRef").on("avl").column("vehicleRef").execute();
-    await db.schema.createIndex("idx_avl_lineRef").on("avl").column("lineRef").execute();
-    await db.schema.createIndex("idx_avl_producerRef").on("avl").column("producerRef").execute();
-    await db.schema.createIndex("idx_avl_originRef").on("avl").column("originRef").execute();
-    await db.schema.createIndex("idx_avl_destinationRef").on("avl").column("destinationRef").execute();
-    await db.schema.createIndex("idx_avl_subscriptionId").on("avl").column("subscriptionId").execute();
+    await db.schema.createIndex("idx_avl_operator_ref").on("avl").column("operator_ref").execute();
+    await db.schema.createIndex("idx_avl_vehicle_ref").on("avl").column("vehicle_ref").execute();
+    await db.schema.createIndex("idx_avl_line_ref").on("avl").column("line_ref").execute();
+    await db.schema.createIndex("idx_avl_producer_ref").on("avl").column("producer_ref").execute();
+    await db.schema.createIndex("idx_avl_origin_ref").on("avl").column("origin_ref").execute();
+    await db.schema.createIndex("idx_avl_destination_ref").on("avl").column("destination_ref").execute();
+    await db.schema.createIndex("idx_avl_subscription_id").on("avl").column("subscription_id").execute();
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
-    await db.schema.dropIndex("idx_avl_operatorRef").execute();
-    await db.schema.dropIndex("idx_avl_vehicleRef").execute();
-    await db.schema.dropIndex("idx_avl_lineRef").execute();
-    await db.schema.dropIndex("idx_avl_producerRef").execute();
-    await db.schema.dropIndex("idx_avl_originRef").execute();
-    await db.schema.dropIndex("idx_avl_destinationRef").execute();
-    await db.schema.dropIndex("idx_avl_subscriptionId").execute();
+    await db.schema.dropIndex("idx_avl_operator_ref").execute();
+    await db.schema.dropIndex("idx_avl_vehicle_ref").execute();
+    await db.schema.dropIndex("idx_avl_line_ref").execute();
+    await db.schema.dropIndex("idx_avl_producer_ref").execute();
+    await db.schema.dropIndex("idx_avl_origin_ref").execute();
+    await db.schema.dropIndex("idx_avl_destination_ref").execute();
+    await db.schema.dropIndex("idx_avl_subscription_id").execute();
 }
