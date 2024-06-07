b40cfc09d592b05147a7cddbc884c5561ca75da6
Deanna Sharma <deanna.sharma@doza.consulting>
2024-06-07 19:26:41 +0100

    tf lint fixes

--- terraform/modules/database/aurora-db/variables.tf
+++ terraform/modules/database/aurora-db/variables.tf
@@ -1,45 +1,39 @@
 variable "environment" {
-  type        = string
-  description = "Environment"
+    type        = string
+    description = "Environment"
 }
 
 variable "db_subnet_ids" {
-  type        = list(string)
-  description = "List of subnet IDs"
+    type        = list(string)
+    description = "List of subnet IDs"
 }
 
 variable "vpc_id" {
-  type        = string
-  description = "VPC ID"
+    type        = string
+    description = "VPC ID"
 }
 
 variable "multi_az" {
-  type        = bool
-  description = "Deploy multiple instances across 2 AZs"
-  default     = false
+    type        = bool
+    description = "Deploy multiple instances across 2 AZs"
+    default     = false
 }
 
 variable "instance_class" {
-  type = string
+    type = string
 }
 
 variable "enable_deletion_protection" {
-  type    = bool
-  default = false
+    type    = bool
+    default = false
 }
 
 variable "private_hosted_zone_id" {
-  type        = string
-  description = "ID for the private hosted zone"
+    type        = string
+    description = "ID for the private hosted zone"
 }
 
 variable "private_hosted_zone_name" {
-  type        = string
-  description = "Name of the private hosted zone"
-}
-
-variable "enable_rds_proxy" {
-  type        = bool
-  default     = false
-  description = "Whether to enable RDS proxy or not"
+    type        = string
+    description = "Name of the private hosted zone"
 }
