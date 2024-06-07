31ec5076060282dfab5a9c4cf3db24031bfca1cb
Nathan Betts <nathan.betts@doza.consulting>
2024-06-07 17:02:22 +0100

    remove avl table cleardown from bods task

--- src/tasks/bods-avl-processor/jobs/avl-cleardown.ts
+++ src/tasks/bods-avl-processor/jobs/avl-cleardown.ts
@@ -5,24 +5,18 @@ import Pino from "pino";
 const logger = Pino();
 
 void (async () => {
-    console.time("avl-cleardown");
-
     const dbClient = await getDatabaseClient(process.env.STAGE === "local");
 
     try {
-        const result = await Promise.all([
-            dbClient.deleteFrom("avl").where("valid_until_time", "<", sql<string>`NOW()`).executeTakeFirst(),
-            dbClient.deleteFrom("avl_bods").where("valid_until_time", "<", sql<string>`NOW()`).executeTakeFirst(),
-        ]);
+        const result = await dbClient
+            .deleteFrom("avl_bods")
+            .where("valid_until_time", "<", sql<string>`NOW()`)
+            .executeTakeFirst();
 
-        logger.info(
-            `AVL cleardown successful: deleted ${result[0].numDeletedRows} avl rows, ${result[1].numDeletedRows} avl_bods rows`,
-        );
-
-        console.timeEnd("avl-cleardown");
+        logger.info(`AVL bods cleardown successful: deleted ${result.numDeletedRows} rows`);
     } catch (e) {
         if (e instanceof Error) {
-            logger.error("There was a problem with the AVL cleardown", e);
+            logger.error("There was a problem with the AVL bods cleardown", e);
         }
 
         throw e;
