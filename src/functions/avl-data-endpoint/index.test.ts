1b872deb564a66015878c10e320e9a6c9251eebe
Nathan Betts <nathan.betts@doza.consulting>
2024-06-10 15:18:16 +0100

    add lastAvlDataReceivedDateTime when processing AVL

--- src/functions/avl-data-endpoint/index.test.ts
+++ src/functions/avl-data-endpoint/index.test.ts
@@ -1,5 +1,6 @@
 import * as dynamo from "@bods-integrated-data/shared/dynamo";
 import * as s3 from "@bods-integrated-data/shared/s3";
+import { AvlSubscription } from "@bods-integrated-data/shared/schema/avl-subscribe.schema";
 import { APIGatewayEvent } from "aws-lambda";
 import MockDate from "mockdate";
 import { afterAll, afterEach, beforeAll, describe, expect, it, vi } from "vitest";
@@ -36,12 +37,31 @@ describe("AVL-data-endpoint", () => {
     const mockSubscriptionId = "411e4495-4a57-4d2f-89d5-cf105441f321";
 
     it("Should add valid AVL data to S3", async () => {
+        getDynamoItemSpy.mockResolvedValue({
+            PK: "411e4495-4a57-4d2f-89d5-cf105441f321",
+            url: "https://mock-data-producer.com/",
+            description: "test-description",
+            shortDescription: "test-short-description",
+            status: "ACTIVE",
+            requestorRef: null,
+        });
+
         const mockEvent = {
             body: testSiri,
             pathParameters: {
                 subscription_id: mockSubscriptionId,
             },
         } as unknown as APIGatewayEvent;
+
+        const expectedSubscription: AvlSubscription = {
+            PK: "411e4495-4a57-4d2f-89d5-cf105441f321",
+            description: "test-description",
+            lastAvlDataReceivedDateTime: "2024-03-11T15:20:02.093Z",
+            requestorRef: null,
+            shortDescription: "test-short-description",
+            status: "ACTIVE",
+            url: "https://mock-data-producer.com/",
+        };
 
         await expect(handler(mockEvent)).resolves.toEqual({ statusCode: 200 });
         expect(s3.putS3Object).toBeCalled();
@@ -51,9 +71,26 @@ describe("AVL-data-endpoint", () => {
             ContentType: "application/xml",
             Key: `${mockSubscriptionId}/2024-03-11T15:20:02.093Z.xml`,
         });
+
+        expect(dynamo.putDynamoItem).toBeCalledWith<Parameters<typeof dynamo.putDynamoItem>>(
+            "test-dynamodb",
+            expectedSubscription.PK,
+            "SUBSCRIPTION",
+            expectedSubscription,
+        );
     });
 
     it("Should add valid AVL data with a single vehicle activity to S3", async () => {
+        getDynamoItemSpy.mockResolvedValue({
+            PK: "411e4495-4a57-4d2f-89d5-cf105441f321",
+            url: "https://mock-data-producer.com/",
+            description: "test-description",
+            shortDescription: "test-short-description",
+            lastAvlDataReceivedDateTime: "2024-03-11T15:20:02.093Z",
+            status: "ACTIVE",
+            requestorRef: null,
+        });
+
         const mockEvent = {
             body: testSiriWithSingleVehicleActivity,
             pathParameters: {
@@ -83,6 +120,16 @@ describe("AVL-data-endpoint", () => {
     });
 
     it("Should throw an error if invalid XML is parsed", async () => {
+        getDynamoItemSpy.mockResolvedValue({
+            PK: "411e4495-4a57-4d2f-89d5-cf105441f321",
+            url: "https://mock-data-producer.com/",
+            description: "test-description",
+            shortDescription: "test-short-description",
+            lastAvlDataReceivedDateTime: "2024-03-11T15:20:02.093Z",
+            status: "ACTIVE",
+            requestorRef: null,
+        });
+
         const mockEvent = {
             body: "abc",
             pathParameters: {
@@ -111,20 +158,22 @@ describe("AVL-data-endpoint", () => {
             },
         } as unknown as APIGatewayEvent;
 
+        const expectedSubscription: AvlSubscription = {
+            PK: "411e4495-4a57-4d2f-89d5-cf105441f321",
+            description: "test-description",
+            heartbeatLastReceivedDateTime: "2024-04-15T13:25:00+01:00",
+            requestorRef: null,
+            shortDescription: "test-short-description",
+            status: "ACTIVE",
+            url: "https://mock-data-producer.com/",
+        };
+
         await expect(handler(mockEvent)).resolves.toEqual({ statusCode: 200 });
-        expect(dynamo.putDynamoItem).toBeCalledWith(
+        expect(dynamo.putDynamoItem).toBeCalledWith<Parameters<typeof dynamo.putDynamoItem>>(
             "test-dynamodb",
-            "411e4495-4a57-4d2f-89d5-cf105441f321",
+            expectedSubscription.PK,
             "SUBSCRIPTION",
-            {
-                PK: "411e4495-4a57-4d2f-89d5-cf105441f321",
-                description: "test-description",
-                heartbeatLastReceivedDateTime: "2024-04-15T13:25:00+01:00",
-                requestorRef: null,
-                shortDescription: "test-short-description",
-                status: "ACTIVE",
-                url: "https://mock-data-producer.com/",
-            },
+            expectedSubscription,
         );
     });
 
