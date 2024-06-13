8ea84a7b124f045ce314e54ee981ecf685927920
mohit-kpmg <120669547+mohit-kpmg@users.noreply.github.com>
2024-06-13 14:16:34 +0100

    Added shared services folder (#186)
    * Added shared services folder
    
    * Updated as per comments
    
    * Updated as per comments

---
+++ terraform/shared-services/remote-state/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_version = ">= 1.6.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.33"
+    }
+  }
+}
+
+
+module "tf_state" {
+  source = "../../modules/bootstrap/tf-state"
+
+  environment = local.env
+}
+
+import {
+  to = module.tf_state.aws_s3_bucket.terraform_state
+  id = local.bucket_name
+}
+
+import {
+  to = module.tf_state.aws_s3_bucket_versioning.terraform_state
+  id = local.bucket_name
+}
+
+import {
+  to = module.tf_state.aws_s3_bucket_public_access_block.public_access
+  id = local.bucket_name
+}
+
+import {
+  to = module.tf_state.aws_dynamodb_table.terraform_state_lock
+  id = local.dynamodb_table_name
+}
+
+locals {
+  bucket_name         = "integrated-data-tfstate-shared-services"
+  dynamodb_table_name = "integrated-data-state-lock-shared-services"
+  env                 = "shared-services"
+}
