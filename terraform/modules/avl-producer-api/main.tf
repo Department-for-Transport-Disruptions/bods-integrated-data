f1ebbb25ede5bd8f1f84db3ed7858bad29d338a9
Deanna Sharma <83244002+DeannaSharma@users.noreply.github.com>
2024-06-27 13:56:33 +0100

    Fix/ Correct update endpoint env var (#238)
    * correct update endpoint env var
    
    * lint

--- terraform/modules/avl-producer-api/main.tf
+++ terraform/modules/avl-producer-api/main.tf
@@ -88,8 +88,8 @@ module "avl_update_endpoint" {
   avl_mock_data_producer_subscribe_endpoint = (var.environment == "local" ?
     module.avl_mock_data_producer.subscribe_function_url :
   "${module.avl_mock_data_producer.endpoint}/subscribe")
-  avl_data_endpoint = (var.environment == "local" ? "https://www.mock-data-endpoint.com/data" :
-  "https://${module.avl_producer_api_gateway[0].endpoint}/data")
+  avl_data_endpoint = (var.environment == "local" ? "https://www.mock-data-endpoint.com/subscriptions" :
+  "https://${module.avl_producer_api_gateway[0].endpoint}/subscriptions")
   aws_account_id = var.aws_account_id
   aws_region     = var.aws_region
   environment    = var.environment
