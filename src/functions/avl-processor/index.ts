0681f056fd6e9abfc72b1a649873eeaadbc40510
mohit-kpmg <mohit.vashishat@kpmg.co.uk>
2024-06-28 09:56:56 +0100

    Updated code for custom metrics

--- src/functions/avl-processor/index.ts
+++ src/functions/avl-processor/index.ts
@@ -1,4 +1,5 @@
 import { logger } from "@baselime/lambda-logger";
+import { Metrics, MetricUnits } from '@aws-lambda-powertools/metrics';
 import { getAvlSubscription, insertAvls, insertAvlsWithOnwardCalls } from "@bods-integrated-data/shared/avl/utils";
 import { KyselyDb, NewAvl, getDatabaseClient } from "@bods-integrated-data/shared/database";
 import { getS3Object } from "@bods-integrated-data/shared/s3";
@@ -7,6 +8,12 @@ import { S3Event, S3EventRecord, SQSEvent } from "aws-lambda";
 import { XMLParser } from "fast-xml-parser";
 
 const arrayProperties = ["VehicleActivity", "OnwardCall"];
+
+const metrics = new Metrics({
+    namespace: 'BODSAVLProcessor',
+    serviceName: 'BODSAVLProcessor-Service',
+  });
+
 
 const parseXml = (xml: string) => {
     const parser = new XMLParser({
@@ -21,9 +28,11 @@ const parseXml = (xml: string) => {
     const parsedJson = siriSchemaTransformed.safeParse(parsedXml.Siri);
 
     if (!parsedJson.success) {
+        
         logger.error("There was an error parsing the AVL data", parsedJson.error.format());
 
         throw new Error("Error parsing data");
+
     }
 
     return parsedJson.data;
@@ -49,7 +58,10 @@ export const processSqsRecord = async (record: S3EventRecord, dbClient: KyselyDb
         const avls = parseXml(await body.transformToString());
 
         if (!avls || avls.length === 0) {
-            throw new Error("Error parsing data");
+            metrics.addDimension('subscriptionID', subscriptionId);
+            metrics.addMetric('invalidSiriSchema', MetricUnits.Count, 1);
+            metrics.publishStoredMetrics();
+            throw new Error("Error parsing data");          
         }
 
         const avlsWithOnwardCalls = avls.filter((avl) => avl.onward_calls);
@@ -84,7 +96,8 @@ export const handler = async (event: SQSEvent) => {
                 ),
             ),
         );
-
+        metrics.addMetric('totalAvlProcessed', MetricUnits.Count,1);
+        metrics.publishStoredMetrics();
         logger.info("AVL uploaded to database successfully");
     } catch (e) {
         if (e instanceof Error) {
