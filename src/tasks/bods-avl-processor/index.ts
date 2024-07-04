bac184e8ebfb12cfcad13dd56bc44222d603d733
Laurence Jones <laurence.jones@doza.consulting>
2024-07-04 09:14:21 +0100

    trigger pipeline again

--- src/tasks/bods-avl-processor/index.ts
+++ src/tasks/bods-avl-processor/index.ts
@@ -11,7 +11,7 @@ const bree = new Bree({
             name: "avl-processor",
             interval: `${process.env.PROCESSOR_FREQUENCY_IN_SECONDS}s`,
             timeout: 0,
-            closeWorkerAfterMs: 30000,
+            closeWorkerAfterMs: 31000,
         },
         {
             name: "avl-cleardown",
