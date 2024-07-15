b2a8509a72e2f83e77391aa382378682e76349dc
Nathan Betts <nathan.betts@doza.consulting>
2024-07-15 08:42:30 +0100

    feat: add ListSecrets permission to CI

--- terraform/modules/bootstrap/oidc/main.tf
+++ terraform/modules/bootstrap/oidc/main.tf
@@ -108,6 +108,7 @@ resource "aws_iam_policy" "integrated_data_oidc_github_actions_policy" {
           "iam:UpdateRole",
           "iam:UpdateRoleDescription",
           "route53:*",
+          "secretsmanager:ListSecrets",
           "secretsmanager:CreateSecret",
           "secretsmanager:GetSecretValue",
           "secretsmanager:PutSecretValue",
