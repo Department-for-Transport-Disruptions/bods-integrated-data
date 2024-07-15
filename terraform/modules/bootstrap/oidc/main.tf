5352dfaf24bfcf4dcd617cda836e738429564c4a
Nathan Betts <nbetts@users.noreply.github.com>
2024-07-15 09:22:06 +0100

    fix: use correct API key header in integration tests (#272)

--- terraform/modules/bootstrap/oidc/main.tf
+++ terraform/modules/bootstrap/oidc/main.tf
@@ -108,15 +108,6 @@ resource "aws_iam_policy" "integrated_data_oidc_github_actions_policy" {
           "iam:UpdateRole",
           "iam:UpdateRoleDescription",
           "route53:*",
-          "secretsmanager:CreateSecret",
-          "secretsmanager:GetSecretValue",
-          "secretsmanager:PutSecretValue",
-          "secretsmanager:UpdateSecret",
-          "secretsmanager:DeleteSecret",
-          "secretsmanager:DescribeSecret",
-          "secretsmanager:TagResource",
-          "secretsmanager:UntagResource",
-          "secretsmanager:GetResourcePolicy",
           "cloudfront:*"
         ]
         Effect   = "Allow"
@@ -139,6 +130,7 @@ resource "aws_iam_policy" "integrated_data_oidc_github_actions_policy" {
           "sns:*",
           "sqs:*",
           "apigateway:*",
+          "secretsmanager:*",
           "states:*",
           "acm:*",
           "scheduler:*",
