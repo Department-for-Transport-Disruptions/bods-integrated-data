7b1798c18f94991ff1da4bef4e9919e6a8cdde69
Tom Kelly <thomas.kelly1@kpmg.co.uk>
2024-07-31 11:50:28 +0100

    BODS-6331: Added data endpoint integration test

--- integration-testing/data/dynamo.ts
+++ integration-testing/data/dynamo.ts
@@ -1,5 +1,5 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DeleteCommand, DynamoDBDocumentClient, NativeAttributeValue } from "@aws-sdk/lib-dynamodb";
+import { DeleteCommand, DynamoDBDocumentClient, GetCommand, NativeAttributeValue } from "@aws-sdk/lib-dynamodb";
 
 const dynamoDbDocClient = DynamoDBDocumentClient.from(
     new DynamoDBClient({
@@ -14,4 +14,18 @@ export const deleteDynamoItem = async (tableName: string, key: Record<string, Na
             Key: key,
         }),
     );
+};
+
+export const getDynamoItem = async <T extends Record<string, unknown>>(
+    tableName: string,
+    key: Record<string, NativeAttributeValue>,
+) => {
+    const data = await dynamoDbDocClient.send(
+        new GetCommand({
+            TableName: tableName,
+            Key: key,
+        }),
+    );
+
+    return data.Item ? (data.Item as T) : null;
 };
--- integration-testing/tests/avl-producer-api.spec.ts
+++ integration-testing/tests/avl-producer-api.spec.ts
@@ -1,6 +1,8 @@
 import { AvlSubscribeMessage } from "@bods-integrated-data/shared/schema/avl-subscribe.schema";
 import { expect, test } from "@playwright/test";
-import { deleteDynamoItem } from "../data/dynamo";
+import { deleteDynamoItem, getDynamoItem } from "../data/dynamo";
+import { generateMockHeartbeat } from "../data/mockHeartbeat";
+import { generateMockSiriVm } from "../data/mockSiri";
 import { getSecretByKey } from "./utils";
 
 const { STAGE: stage } = process.env;
@@ -14,6 +16,9 @@ const avlSubscriptionTableName = `integrated-data-avl-subscription-table-${stage
 const headers = {
     apiKey: "",
 };
+
+const mockTimestamp = "2024-01-01T00:00:00.000";
+const mockEndTimestamp = "2024-04-01T00:00:00.000";
 
 const testSubscription: AvlSubscribeMessage = {
     dataProducerEndpoint: "http://siri.ticketer.org.uk/api/vm",
@@ -31,6 +36,19 @@ const cleardownTestSubscription = async () => {
         PK: testSubscription.subscriptionId,
         SK: "SUBSCRIPTION",
     });
+};
+
+const getTestSubscription = async () => {
+    const dynamoItem = await getDynamoItem(avlSubscriptionTableName, {
+        PK: testSubscription.subscriptionId,
+        SK: "SUBSCRIPTION",
+    });
+
+    if (!dynamoItem) {
+        throw new Error("Subscription not found in dynamo");
+    }
+
+    return dynamoItem;
 };
 
 test.beforeAll(async () => {
@@ -120,6 +138,38 @@ test.describe("avl-producer-api", () => {
                 }),
             ]),
         );
+    });
+
+    test("should post data to data endpoint", async ({ request }) => {
+        const subscription = await getTestSubscription();
+        const mockHeartbeat = generateMockHeartbeat(testSubscription.subscriptionId, mockTimestamp);
+        const mockSiri = generateMockSiriVm(testSubscription.subscriptionId, mockTimestamp, mockEndTimestamp);
+
+        const heartbeatRequest = await request.post(
+            `${avlProducerApiUrl}/subscriptions/${testSubscription.subscriptionId}?apiKey=${subscription.apiKey}`,
+            { headers: { ...headers, "Content-Type": "text/xml" }, data: mockHeartbeat },
+        );
+
+        expect(heartbeatRequest.status()).toBe(200);
+
+        const dataRequest = await request.post(
+            `${avlProducerApiUrl}/subscriptions/${testSubscription.subscriptionId}?apiKey=${subscription.apiKey}`,
+            { headers: { ...headers, "Content-Type": "text/xml" }, data: mockSiri },
+        );
+
+        expect(dataRequest.status()).toBe(200);
+
+        const getSubscriptionResponse = await request.get(
+            `${avlProducerApiUrl}/subscriptions/${testSubscription.subscriptionId}`,
+            { headers },
+        );
+
+        expect(getSubscriptionResponse.status()).toBe(200);
+
+        const responseData = await getSubscriptionResponse.json();
+
+        expect(responseData.lastAvlDataReceivedDateTime).not.toBeNull();
+        expect(responseData.heartbeatLastReceivedDateTime).not.toBeNull();
     });
 
     test("should allow a data producer subscription to be deleted", async ({ request }) => {
