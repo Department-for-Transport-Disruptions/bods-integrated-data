3a8f69cded3b0c47714ed219913966f47ba54b18
tomkellykpmg <119508391+tomkellykpmg@users.noreply.github.com>
2024-08-01 10:29:29 +0100

    BODS-6012: Validate Endpoint (#293)
    Co-authored-by: Nathan Betts <nbetts@users.noreply.github.com>

---
+++ terraform/modules/avl-producer-api/avl-datafeed-validator/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_version = ">= 1.6.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.54"
+    }
+  }
+}
+
+module "avl_datafeed_validator" {
+  source = "../../shared/lambda-function"
+
+  environment   = var.environment
+  function_name = "integrated-data-avl-datafeed-validator"
+  zip_path      = "${path.module}/../../../../src/functions/dist/avl-datafeed-validator.zip"
+  handler       = "index.handler"
+  memory        = 256
+  runtime       = "nodejs20.x"
+  timeout       = 120
+
+  permissions = [
+    {
+      Action   = ["dynamodb:Scan"]
+      Effect   = "Allow",
+      Resource = "arn:aws:dynamodb:${var.aws_region}:${var.aws_account_id}:table/${var.avl_error_table_name}"
+
+    },
+    {
+      Action = [
+        "cloudwatch:GetMetricStatistics"
+      ],
+      Effect   = "Allow",
+      Resource = "*"
+    },
+    {
+      Action = [
+        "secretsmanager:GetSecretValue",
+      ],
+      Effect = "Allow",
+      Resource = [
+        var.avl_producer_api_key_arn
+      ]
+    }
+  ]
+
+  env_vars = {
+    STAGE                      = var.environment
+    AVL_VALIDATION_ERROR_TABLE = var.avl_error_table_name
+    CLOUDWATCH_NAMESPACE       = var.avl_cloudwatch_namespace
+    AVL_PRODUCER_API_KEY_ARN   = var.avl_producer_api_key_arn
+  }
+}
+
+output "function_arn" {
+  value       = module.avl_datafeed_validator.function_arn
+  description = "Function ARN for AVL Datafeed Validator"
+}
