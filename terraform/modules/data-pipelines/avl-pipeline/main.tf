bd6300bebef457ed0b3b09345a6491b8320cc1d4
Nathan Betts <nbetts@users.noreply.github.com>
2024-08-01 15:47:24 +0100

    add batch write permission to avl processor (#303)

--- terraform/modules/data-pipelines/avl-pipeline/main.tf
+++ terraform/modules/data-pipelines/avl-pipeline/main.tf
@@ -68,7 +68,7 @@ module "integrated_data_avl_processor_function" {
       ]
     },
     {
-      Action   = ["dynamodb:GetItem"],
+      Action   = ["dynamodb:GetItem", "dynamodb:BatchWriteItem"],
       Effect   = "Allow",
       Resource = "arn:aws:dynamodb:${var.aws_region}:${var.aws_account_id}:table/${var.avl_subscription_table_name}"
     },
