6105017fee8edf9b9d2fa3c7cb8767cdb23d2384
Tom Kelly <thomas.kelly1@kpmg.co.uk>
2024-08-05 10:27:32 +0100

    Fix timestamp and recursivescan

--- cli-helpers/src/commands/invoke-avl-datafeed-validator.ts
+++ cli-helpers/src/commands/invoke-avl-datafeed-validator.ts
@@ -1,11 +1,13 @@
 import { Command } from "@commander-js/extra-typings";
 import inquirer from "inquirer";
-import { STAGES, STAGE_OPTION, invokeLambda } from "../utils";
+import { STAGES, STAGE_OPTION_WITH_DEFAULT, getSecretByKey, invokeLambda } from "../utils";
 
 export const invokeAvlDataFeedValidator = new Command("invoke-avl-data-feed-validator")
-    .addOption(STAGE_OPTION)
+    .addOption(STAGE_OPTION_WITH_DEFAULT)
+    .option("--subscriptionId <subscriptionId>", "Data producer subscription ID")
     .action(async (options) => {
-        let { stage } = options;
+        let { stage, subscriptionId } = options;
+        const apiKey = await getSecretByKey(stage, "avl_producer_api_key");
 
         if (!stage) {
             const responses = await inquirer.prompt<{ stage: string }>([
@@ -20,8 +22,28 @@ export const invokeAvlDataFeedValidator = new Command("invoke-avl-data-feed-vali
             stage = responses.stage;
         }
 
+        if (!subscriptionId) {
+            const response = await inquirer.prompt<{ subscriptionId: string }>([
+                {
+                    name: "subscriptionId",
+                    message: "Enter the data producer's subscriptionId",
+                    type: "input",
+                },
+            ]);
+
+            subscriptionId = response.subscriptionId;
+        }
+
+        const payload = {
+            headers: {
+                "x-api-key": apiKey,
+            },
+            pathParameters: { subscriptionId: `${subscriptionId}` },
+        };
+
         await invokeLambda(stage, {
             FunctionName: `integrated-data-avl-datafeed-validator-${stage}`,
             InvocationType: "RequestResponse",
+            Payload: JSON.stringify(payload),
         });
     });
