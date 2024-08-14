e05ff4443bb5180ce4298b099ecc3784f11a8ecc
Tom Kelly <thomas.kelly1@kpmg.co.uk>
2024-08-14 14:35:28 +0100

    BODS-5067: Add unzipping to retriever

--- src/functions/bods-disruptions-retriever/index.ts
+++ src/functions/bods-disruptions-retriever/index.ts
@@ -1,43 +1,37 @@
-import { PassThrough, Stream } from "node:stream";
+import { Stream } from "node:stream";
 import { logger, withLambdaRequestTracker } from "@bods-integrated-data/shared/logger";
-import { startS3Upload } from "@bods-integrated-data/shared/s3";
+import { unzip } from "@bods-integrated-data/shared/unzip";
 import { Handler } from "aws-lambda";
 import axios from "axios";
 
-const getDisruptionsDataAndUploadToS3 = async (disruptionsZippedBucketName: string) => {
+const getDisruptionsDataAndUploadToS3 = async (disruptionsUnzippedBucketName: string) => {
     logger.info("Starting retrieval of disruptions data");
 
     const response = await axios.get<Stream>("https://data.bus-data.dft.gov.uk/disruptions/download/bulk_archive", {
         responseType: "stream",
     });
 
-    const passThrough = new PassThrough();
-
-    const upload = startS3Upload(disruptionsZippedBucketName, "disruptions.zip", passThrough, "application/zip");
-
-    response.data.pipe(passThrough);
-
-    await upload.done();
+    await unzip(response.data, "disruptions.zip", disruptionsUnzippedBucketName);
 };
 
 export const handler: Handler = async (event, context) => {
     withLambdaRequestTracker(event ?? {}, context ?? {});
 
-    const { DISRUPTIONS_ZIPPED_BUCKET_NAME: disruptionsZippedBucketName } = process.env;
+    const { DISRUPTIONS_UNZIPPED_BUCKET_NAME: disruptionsUnzippedBucketName } = process.env;
 
-    if (!disruptionsZippedBucketName) {
-        throw new Error("Missing env vars - DISRUPTIONS_ZIPPED_BUCKET_NAME must be set");
+    if (!disruptionsUnzippedBucketName) {
+        throw new Error("Missing env vars - DISRUPTIONS_UNZIPPED_BUCKET_NAME must be set");
     }
 
     try {
         logger.info("Starting retrieval of disruptions data");
 
-        await getDisruptionsDataAndUploadToS3(disruptionsZippedBucketName);
+        await getDisruptionsDataAndUploadToS3(disruptionsUnzippedBucketName);
 
         logger.info("Disruptions retrieval complete");
 
         return {
-            disruptionsZippedBucketName,
+            disruptionsUnzippedBucketName,
         };
     } catch (e) {
         if (e instanceof Error) {
