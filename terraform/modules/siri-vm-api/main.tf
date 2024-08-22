d5c76e9472464602623ddc9984a1ac931b0ece89
Nathan Betts <nathan.betts@doza.consulting>
2024-08-22 09:08:24 +0100

    update path name

--- terraform/modules/siri-vm-api/main.tf
+++ terraform/modules/siri-vm-api/main.tf
@@ -172,7 +172,7 @@ resource "aws_api_gateway_rest_api_policy" "siri_vm_api_resource_policy" {
 resource "aws_api_gateway_resource" "avl_consumer_subscriber_resource" {
   rest_api_id = aws_api_gateway_rest_api.siri_vm_api.id
   parent_id   = aws_api_gateway_rest_api.siri_vm_api.root_resource_id
-  path_part   = "siri-vm"
+  path_part   = "siri-vm/subscriptions"
 }
 
 resource "aws_api_gateway_method" "avl_consumer_subscriber_method" {
