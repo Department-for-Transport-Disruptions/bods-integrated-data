19fd1a20d32716ded51ac67e0253da9de426f4bd
Laurence Jones <laurence.jones@doza.consulting>
2024-08-22 09:08:15 +0100

    fix: allow cancellations data with vehicle activities (#336)

--- src/functions/avl-data-endpoint/testSiriVm.ts
+++ src/functions/avl-data-endpoint/testSiriVm.ts
@@ -147,6 +147,67 @@ export const testCancellationsSiri = `<?xml version="1.0" encoding="UTF-8" stand
 </ServiceDelivery>
 </Siri>`;
 
+export const testVehicleActivityAndCancellationsSiri = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Siri xmlns="http://www.siri.org.uk/siri" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="2.0" xsi:schemaLocation="http://www.siri.org.uk/siri http://www.siri.org.uk/schema/2.0/siri.xsd">
+<ServiceDelivery>
+<ResponseTimestamp>2024-08-08T09:55:06+01:00</ResponseTimestamp>
+<Status>true</Status>
+<VehicleMonitoringDelivery version="2.0">
+<ResponseTimestamp>2024-08-08T09:55:06+01:00</ResponseTimestamp>
+<SubscriberRef>CAVL_Test</SubscriberRef>
+<SubscriptionRef>10609</SubscriptionRef>
+<Status>true</Status>
+<ValidUntil>2024-08-08T09:55:06+01:00</ValidUntil>
+<ShortestPossibleCycle>PT30S</ShortestPossibleCycle>
+<VehicleActivity>
+<RecordedAtTime>2018-08-17T15:13:20</RecordedAtTime>
+<ValidUntilTime>2018-08-17T16:13:29</ValidUntilTime>
+<MonitoredVehicleJourney>
+<LineRef>ATB:Line:60</LineRef>
+<DirectionRef>2</DirectionRef>
+<OperatorRef>placeholder</OperatorRef>
+<FramedVehicleJourneyRef>
+<DataFrameRef>2018-08-17</DataFrameRef>
+<DatedVehicleJourneyRef>ATB:ServiceJourney:00600027</DatedVehicleJourneyRef>
+</FramedVehicleJourneyRef>
+<VehicleRef>200141</VehicleRef>
+<Bearing>0</Bearing>
+<VehicleLocation>
+<Longitude>10.40261</Longitude>
+<Latitude>63.43613</Latitude>
+</VehicleLocation>
+<BlockRef>blockRef</BlockRef>
+<OriginRef>originRef</OriginRef>
+<DestinationRef>destinationRef</DestinationRef>
+<PublishedLineName>1</PublishedLineName>
+</MonitoredVehicleJourney>
+</VehicleActivity>
+<VehicleActivityCancellation>
+<RecordedAtTime>2024-08-08T09:55:00+01:00</RecordedAtTime>
+<ItemRef>CAVL_TEST:VEHICLESTATUSRT:2598:15329</ItemRef>
+<VehicleMonitoringRef>TCVW-E159</VehicleMonitoringRef>
+<VehicleJourneyRef>
+<DataFrameRef>2024-08-08</DataFrameRef>
+<DatedVehicleJourneyRef>31</DatedVehicleJourneyRef>
+</VehicleJourneyRef>
+<LineRef>17</LineRef>
+<DirectionRef>OUTBOUND</DirectionRef>
+</VehicleActivityCancellation>
+<VehicleActivityCancellation>
+<RecordedAtTime>2024-08-08T09:55:00+01:00</RecordedAtTime>
+<ItemRef>CAVL_TEST:VEHICLESTATUSRT:2590:14024</ItemRef>
+<VehicleMonitoringRef>TCVW-E151</VehicleMonitoringRef>
+<VehicleJourneyRef>
+<DataFrameRef>2024-08-08</DataFrameRef>
+<DatedVehicleJourneyRef>27</DatedVehicleJourneyRef>
+</VehicleJourneyRef>
+<LineRef>13</LineRef>
+<DirectionRef>OUTBOUND</DirectionRef>
+</VehicleActivityCancellation>
+</VehicleMonitoringDelivery>
+</ServiceDelivery>
+</Siri>`;
+
 export const testSiriWithNoVehicleActivity = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <Siri xmlns="http://www.siri.org.uk/siri"
     xmlns:ns2="http://www.ifopt.org.uk/acsb"
