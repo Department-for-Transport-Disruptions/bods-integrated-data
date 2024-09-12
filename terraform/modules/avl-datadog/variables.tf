2b7c2870f8f802379e5e328ef4ec041f4f17a23c
mohit-kpmg <mohit.vashishat@kpmg.co.uk>
2024-09-12 22:51:41 +0100

    Remved keys

--- terraform/modules/avl-datadog/variables.tf
+++ terraform/modules/avl-datadog/variables.tf
@@ -12,13 +12,13 @@ variable "datadog_api_url" {
 variable "datadog_api_key" {
   type        = string
   description = "API key required by DataDog to submit metrics and events"
-  default     = "6a2cec651f19bdd0cb83ff13aab935bf"
+  default     = ""
 }
 
 variable "datadog_app_key" {
   type        = string
   description = "Used in conjunction with the API key to give users access to DataDog's programmatic API"
-  default     = "5057666642b00ed856d4a1e532ce8942f2a4d6ad"
+  default     = ""
 }
 
 variable "datadog_external_id" {
