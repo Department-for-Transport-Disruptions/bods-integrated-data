63a15b3a63caf769639060342d81b72993dfb3b4
Deanna Sharma <83244002+DeannaSharma@users.noreply.github.com>
2024-10-01 14:55:11 +0100

    fix: add data endpoint to api gateway (#415)

--- terraform/modules/avl-producer-api/avl-data-endpoint/outputs.tf
+++ terraform/modules/avl-producer-api/avl-data-endpoint/outputs.tf
@@ -6,3 +6,14 @@ output "lambda_name" {
 output "invoke_arn" {
   value = module.integrated_data_bods_avl_data_endpoint_function.invoke_arn
 }
+
+
+output "lambda_arn" {
+  description = "Lambda ARN"
+  value       = module.integrated_data_bods_avl_data_endpoint_function.lambda_arn
+}
+
+output "function_name" {
+  description = "Lambda function name"
+  value       = module.integrated_data_bods_avl_data_endpoint_function.function_name
+}
