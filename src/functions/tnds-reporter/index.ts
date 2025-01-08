37f2f5bcc3da866e5ab04ab19a71317efe471379
Nathan Betts <13869955+nbetts@users.noreply.github.com>
2025-01-08 14:26:24 +0000

    feat: use prefix in step function

--- src/functions/tnds-reporter/index.ts
+++ src/functions/tnds-reporter/index.ts
@@ -1,4 +1,3 @@
-import { getDate } from "@bods-integrated-data/shared/dates";
 import { recursiveScan } from "@bods-integrated-data/shared/dynamo";
 import { errorMapWithDataLogging, logger, withLambdaRequestTracker } from "@bods-integrated-data/shared/logger";
 import { putS3Object } from "@bods-integrated-data/shared/s3";
@@ -29,8 +28,6 @@ export const handler: Handler = async (event, context) => {
         throw new Error("Missing env vars - TNDS_ANALYSIS_TABLE_NAME and TNDS_ANALYSIS_BUCKET_NAME must be set");
     }
 
-    logger.info(`temp log - event info: ${JSON.stringify(event)}`);
-
     const observations = await getObservations(TNDS_ANALYSIS_TABLE_NAME);
     logger.info(`Creating report with ${observations.length} observations`);
 
@@ -60,11 +57,9 @@ export const handler: Handler = async (event, context) => {
         csvContent += `${csvRows.map((row) => Object.values(row).join(",")).join("\r\n")}\r\n`;
     }
 
-    const date = getDate().format("YYYYMMDD");
-
     await putS3Object({
         Bucket: TNDS_ANALYSIS_BUCKET_NAME,
-        Key: `${date}.csv`,
+        Key: `${event.prefix}.csv`,
         ContentType: "text/csv",
         Body: csvContent,
     });
