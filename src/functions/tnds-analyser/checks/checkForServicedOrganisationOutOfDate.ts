b99eb2f388a1ac997dad83dbce8e4312762806a4
Nathan Betts <nbetts@users.noreply.github.com>
2025-01-08 10:58:13 +0000

    feat: serviced organisation out of date check (#500)
    Co-authored-by: Nathan Betts <13869955+nbetts@users.noreply.github.com>

---
+++ src/functions/tnds-analyser/checks/checkForServicedOrganisationOutOfDate.ts
@@ -0,0 +1,51 @@
+import { randomUUID } from "node:crypto";
+import { getDate } from "@bods-integrated-data/shared/dates";
+import { TxcSchema } from "@bods-integrated-data/shared/schema";
+import { Observation } from "@bods-integrated-data/shared/tnds-analyser/schema";
+
+export default (filename: string, data: Partial<TxcSchema>): Observation[] => {
+    const observations: Observation[] = [];
+    const services = data.TransXChange?.Services?.Service;
+
+    if (services) {
+        for (const service of services) {
+            const servicedOrganisationRefs =
+                service.OperatingProfile?.ServicedOrganisationDayType?.DaysOfOperation?.WorkingDays
+                    ?.ServicedOrganisationRef;
+
+            if (servicedOrganisationRefs) {
+                for (const servicedOrganisationRef of servicedOrganisationRefs) {
+                    const servicedOrganisation = data.TransXChange?.ServicedOrganisations?.ServicedOrganisation?.find(
+                        (servicedOrganisation) => servicedOrganisation.OrganisationCode === servicedOrganisationRef,
+                    );
+
+                    const dateRanges = servicedOrganisation?.WorkingDays?.DateRange;
+
+                    if (dateRanges) {
+                        const sortedEndDates = dateRanges.map(([, endDate]) => endDate).sort((a, b) => a.diff(b));
+                        const latestEndDate = sortedEndDates[sortedEndDates.length - 1];
+                        const now = getDate();
+
+                        if (latestEndDate.isBefore(now)) {
+                            const serviceName = servicedOrganisation.Name || "unknown";
+                            const endDate = latestEndDate.format("YYYY-MM-DD");
+
+                            observations.push({
+                                PK: filename,
+                                SK: randomUUID(),
+                                importance: "advisory",
+                                category: "dataset",
+                                observation: "Serviced organisation out of date",
+                                registrationNumber: service.ServiceCode,
+                                service: "n/a",
+                                details: `The Working Days for Serviced Organisation ${serviceName} (${servicedOrganisationRef}) has expired on ${endDate}. Please update the dates for this Serviced Organisation.`,
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return observations;
+};
