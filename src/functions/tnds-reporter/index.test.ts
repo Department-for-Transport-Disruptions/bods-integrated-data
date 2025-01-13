dab406d78ec694f0533b7749ab095a6c1edeb93a
Nathan Betts <13869955+nbetts@users.noreply.github.com>
2025-01-13 13:49:11 +0000

    feat: report includes breakdowns by observation type

--- src/functions/tnds-reporter/index.test.ts
+++ src/functions/tnds-reporter/index.test.ts
@@ -1,14 +1,13 @@
 import * as dynamo from "@bods-integrated-data/shared/dynamo";
-import { logger } from "@bods-integrated-data/shared/logger";
 import { mockCallback, mockContext } from "@bods-integrated-data/shared/mockHandlerArgs";
 import { putS3Object } from "@bods-integrated-data/shared/s3";
-import { Observation } from "@bods-integrated-data/shared/tnds-analyser/schema";
+import { ObservationSummary } from "@bods-integrated-data/shared/tnds-analyser/schema";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import { handler } from "./index";
 
 describe("tnds-reporter", () => {
     const mockEvent = { prefix: "20250108" };
-    const recursiveScanSpy = vi.spyOn(dynamo, "recursiveScan");
+    const scanDynamoSpy = vi.spyOn(dynamo, "scanDynamo");
 
     vi.mock("@bods-integrated-data/shared/s3", () => ({
         putS3Object: vi.fn(),
@@ -25,55 +24,147 @@ describe("tnds-reporter", () => {
 
     beforeEach(() => {
         vi.clearAllMocks();
-        process.env.TNDS_ANALYSIS_TABLE_NAME = "test-table";
+        process.env.TNDS_OBSERVATION_TABLE_NAME = "test-table";
         process.env.TNDS_ANALYSIS_BUCKET_NAME = "test-bucket";
     });
 
     it.each([
-        [{ TNDS_ANALYSIS_TABLE_NAME: "", TNDS_ANALYSIS_BUCKET_NAME: "test-bucket" }],
-        [{ TNDS_ANALYSIS_TABLE_NAME: "test-table", TNDS_ANALYSIS_BUCKET_NAME: "" }],
+        [{ TNDS_OBSERVATION_TABLE_NAME: "", TNDS_ANALYSIS_BUCKET_NAME: "test-bucket" }],
+        [{ TNDS_OBSERVATION_TABLE_NAME: "test-table", TNDS_ANALYSIS_BUCKET_NAME: "" }],
     ])("throws an error when the required env vars are missing", async (env) => {
         process.env = env;
 
         await expect(handler(mockEvent, mockContext, mockCallback)).rejects.toThrow(
-            "Missing env vars - TNDS_ANALYSIS_TABLE_NAME and TNDS_ANALYSIS_BUCKET_NAME must be set",
+            "Missing env vars - TNDS_OBSERVATION_TABLE_NAME and TNDS_ANALYSIS_BUCKET_NAME must be set",
         );
     });
 
     it("creates a report and uploads it to S3", async () => {
-        const mockObservations: Observation[] = [
+        const mockObservationSummaries: ObservationSummary[] = [
             {
-                PK: "file1",
-                SK: "a5764857-ae35-34dc-8f25-a9c9e73aa898",
-                importance: "critical",
-                category: "journey",
-                observation: "Duplicate journey code",
-                registrationNumber: "reg1",
-                service: "service1",
-                details: "details1",
+                File: "file1",
+                "Data Source": "bods",
+                "Total observations": 2,
+                "Critical observations": 2,
+                "Advisory observations": 2,
+                "No timing point for more than 15 minutes": 2,
+                "First stop is not a timing point": 2,
+                "Last stop is not a timing point": 2,
+                "Last stop is pick up only": 2,
+                "First stop is set down only": 2,
+                "Stop not found in NaPTAN": 2,
+                "Incorrect stop type": 2,
+                "Missing journey code": 2,
+                "Duplicate journey code": 2,
+                "Duplicate journey": 2,
+                "Missing bus working number": 2,
+                "Serviced organisation out of date": 2,
             },
             {
-                PK: "file2",
-                SK: "a5764857-ae35-34dc-8f25-a9c9e73aa899",
-                importance: "advisory",
-                category: "dataset",
-                observation: "Serviced organisation out of date",
-                registrationNumber: "reg2",
-                service: "service2",
-                details: "details2",
+                File: "file2",
+                "Data Source": "tnds",
+                "Total observations": 3,
+                "Critical observations": 3,
+                "Advisory observations": 3,
+                "No timing point for more than 15 minutes": 3,
+                "First stop is not a timing point": 3,
+                "Last stop is not a timing point": 3,
+                "Last stop is pick up only": 3,
+                "First stop is set down only": 3,
+                "Stop not found in NaPTAN": 3,
+                "Incorrect stop type": 3,
+                "Missing journey code": 3,
+                "Duplicate journey code": 3,
+                "Duplicate journey": 3,
+                "Missing bus working number": 3,
+                "Serviced organisation out of date": 3,
+            },
+            {
+                File: "file3",
+                "Data Source": "tnds",
+                "Total observations": 5,
+                "Critical observations": 5,
+                "Advisory observations": 5,
+                "No timing point for more than 15 minutes": 5,
+                "First stop is not a timing point": 5,
+                "Last stop is not a timing point": 5,
+                "Last stop is pick up only": 5,
+                "First stop is set down only": 5,
+                "Stop not found in NaPTAN": 5,
+                "Incorrect stop type": 5,
+                "Missing journey code": 5,
+                "Duplicate journey code": 5,
+                "Duplicate journey": 5,
+                "Missing bus working number": 5,
+                "Serviced organisation out of date": 5,
             },
         ];
 
-        const csvContent = `filename,importance,category,observation,registrationNumber,service,details\r
-file1,critical,journey,Duplicate journey code,reg1,service1,details1\r
-file2,advisory,dataset,Serviced organisation out of date,reg2,service2,details2\r
-`;
+        const row1 = [
+            "Data Source",
+            "Total observations",
+            "Critical observations",
+            "Advisory observations",
+            "No timing point for more than 15 minutes",
+            "First stop is not a timing point",
+            "Last stop is not a timing point",
+            "Last stop is pick up only",
+            "First stop is set down only",
+            "Stop not found in NaPTAN",
+            "Incorrect stop type",
+            "Missing journey code",
+            "Duplicate journey code",
+            "Duplicate journey",
+            "Missing bus working number",
+            "Serviced organisation out of date",
+        ];
 
-        recursiveScanSpy.mockResolvedValueOnce(mockObservations);
+        const row2: Omit<ObservationSummary, "File"> = {
+            "Data Source": "bods",
+            "Total observations": 2,
+            "Critical observations": 2,
+            "Advisory observations": 2,
+            "No timing point for more than 15 minutes": 2,
+            "First stop is not a timing point": 2,
+            "Last stop is not a timing point": 2,
+            "Last stop is pick up only": 2,
+            "First stop is set down only": 2,
+            "Stop not found in NaPTAN": 2,
+            "Incorrect stop type": 2,
+            "Missing journey code": 2,
+            "Duplicate journey code": 2,
+            "Duplicate journey": 2,
+            "Missing bus working number": 2,
+            "Serviced organisation out of date": 2,
+        };
+
+        const row3: Omit<ObservationSummary, "File"> = {
+            "Data Source": "tnds",
+            "Total observations": 8,
+            "Critical observations": 8,
+            "Advisory observations": 8,
+            "No timing point for more than 15 minutes": 8,
+            "First stop is not a timing point": 8,
+            "Last stop is not a timing point": 8,
+            "Last stop is pick up only": 8,
+            "First stop is set down only": 8,
+            "Stop not found in NaPTAN": 8,
+            "Incorrect stop type": 8,
+            "Missing journey code": 8,
+            "Duplicate journey code": 8,
+            "Duplicate journey": 8,
+            "Missing bus working number": 8,
+            "Serviced organisation out of date": 8,
+        };
+
+        const csvContent = `${row1}\r\n${Object.values(row2).join(",")}\r\n${Object.values(row3).join(",")}\r\n`;
+
+        scanDynamoSpy.mockResolvedValueOnce({ Items: mockObservationSummaries } as unknown as Awaited<
+            ReturnType<typeof dynamo.scanDynamo>
+        >);
 
         await handler(mockEvent, mockContext, mockCallback);
 
-        expect(logger.info).toHaveBeenCalledWith("Creating report with 2 observations");
         expect(putS3Object).toHaveBeenCalledWith({
             Bucket: "test-bucket",
             Key: "20250108.csv",
@@ -82,18 +173,9 @@ file2,advisory,dataset,Serviced organisation out of date,reg2,service2,details2\
         });
     });
 
-    it("creates an empty report when there are no observations", async () => {
-        const csvContent = "filename,importance,category,observation,registrationNumber,service,details\r\n";
-        recursiveScanSpy.mockResolvedValueOnce(undefined as unknown as Observation[]);
-
+    it("doesn't create a report when there are no observation summaries", async () => {
+        scanDynamoSpy.mockResolvedValueOnce({} as Awaited<ReturnType<typeof dynamo.scanDynamo>>);
         await handler(mockEvent, mockContext, mockCallback);
-
-        expect(logger.info).toHaveBeenCalledWith("Creating report with 0 observations");
-        expect(putS3Object).toHaveBeenCalledWith({
-            Bucket: "test-bucket",
-            Key: "20250108.csv",
-            ContentType: "text/csv",
-            Body: csvContent,
-        });
+        expect(putS3Object).not.toHaveBeenCalled();
     });
 });
--- src/functions/tnds-reporter/index.ts
+++ src/functions/tnds-reporter/index.ts
@@ -1,66 +1,242 @@
-import { recursiveScan } from "@bods-integrated-data/shared/dynamo";
+import { PassThrough } from "node:stream";
+import { getDate } from "@bods-integrated-data/shared/dates";
+import { scanDynamo } from "@bods-integrated-data/shared/dynamo";
 import { errorMapWithDataLogging, logger, withLambdaRequestTracker } from "@bods-integrated-data/shared/logger";
-import { putS3Object } from "@bods-integrated-data/shared/s3";
-import { observationsSchema } from "@bods-integrated-data/shared/tnds-analyser/schema";
+import { startS3Upload } from "@bods-integrated-data/shared/s3";
+import { observationType } from "@bods-integrated-data/shared/tnds-analyser/constants";
+import { observationSchema } from "@bods-integrated-data/shared/tnds-analyser/schema";
+import archiver from "archiver";
 import { Handler } from "aws-lambda";
 import { z } from "zod";
 
 z.setErrorMap(errorMapWithDataLogging);
 
-const getObservations = async (tableName: string) => {
-    const observations = await recursiveScan({
-        TableName: tableName,
-    });
+type ObservationSummaryByDataSource = {
+    "Dataset Date": string;
+    "Data Source": string;
+    "Total observations": number;
+    "Critical observations": number;
+    "Advisory observations": number;
+    "No timing point for more than 15 minutes": number;
+    "First stop is not a timing point": number;
+    "Last stop is not a timing point": number;
+    "Last stop is pick up only": number;
+    "First stop is set down only": number;
+    "Stop not found in NaPTAN": number;
+    "Incorrect stop type": number;
+    "Missing journey code": number;
+    "Duplicate journey code": number;
+    "Duplicate journey": number;
+    "Missing bus working number": number;
+    "Serviced organisation out of date": number;
+};
 
-    if (!observations) {
-        return [];
+type ObservationSummaryByFile = {
+    "Dataset Date": string;
+    Region: string;
+    File: string;
+    "Data Source": string;
+    "Total observations": number;
+    "Critical observations": number;
+    "Advisory observations": number;
+    "No timing point for more than 15 minutes": number;
+    "First stop is not a timing point": number;
+    "Last stop is not a timing point": number;
+    "Last stop is pick up only": number;
+    "First stop is set down only": number;
+    "Stop not found in NaPTAN": number;
+    "Incorrect stop type": number;
+    "Missing journey code": number;
+    "Duplicate journey code": number;
+    "Duplicate journey": number;
+    "Missing bus working number": number;
+    "Serviced organisation out of date": number;
+};
+
+type ObservationSummaryByObservationType = {
+    "Dataset Date": string;
+    Region: string;
+    File: string;
+    "National Operator Code": string;
+    "Line Name": string;
+    Quantity: number;
+};
+
+const createCsv = <T extends Record<string, U>, U>(data: T[]) => {
+    if (!data.length) {
+        return null;
     }
 
-    return observationsSchema.parse(observations);
+    return `${Object.keys(data[0]).join(",")}\r\n${data.map((row) => Object.values(row).join(",")).join("\r\n")}\r\n`;
 };
 
 export const handler: Handler = async (event, context) => {
     withLambdaRequestTracker(event ?? {}, context ?? {});
 
-    const { TNDS_ANALYSIS_TABLE_NAME, TNDS_ANALYSIS_BUCKET_NAME } = process.env;
+    const { TNDS_OBSERVATION_TABLE_NAME, TNDS_ANALYSIS_BUCKET_NAME } = process.env;
 
-    if (!TNDS_ANALYSIS_TABLE_NAME || !TNDS_ANALYSIS_BUCKET_NAME) {
-        throw new Error("Missing env vars - TNDS_ANALYSIS_TABLE_NAME and TNDS_ANALYSIS_BUCKET_NAME must be set");
+    if (!TNDS_OBSERVATION_TABLE_NAME || !TNDS_ANALYSIS_BUCKET_NAME) {
+        throw new Error("Missing env vars - TNDS_OBSERVATION_TABLE_NAME and TNDS_ANALYSIS_BUCKET_NAME must be set");
     }
 
-    const observations = await getObservations(TNDS_ANALYSIS_TABLE_NAME);
-    logger.info(`Creating report with ${observations.length} observations`);
+    const date = event.date;
+    const formattedDate = getDate(date).format("DD/MM/YYYY");
+    const observationByDataSourceMap: Record<string, ObservationSummaryByDataSource> = {};
+    const observationByFileMap: Record<string, ObservationSummaryByFile> = {};
+    const observationByObservationTypesMap: Record<
+        (typeof observationType)[number],
+        Record<string, ObservationSummaryByObservationType>
+    > = {
+        "No timing point for more than 15 minutes": {},
+        "First stop is not a timing point": {},
+        "Last stop is not a timing point": {},
+        "Last stop is pick up only": {},
+        "First stop is set down only": {},
+        "Stop not found in NaPTAN": {},
+        "Incorrect stop type": {},
+        "Missing journey code": {},
+        "Duplicate journey code": {},
+        "Duplicate journey": {},
+        "Missing bus working number": {},
+        "Serviced organisation out of date": {},
+    };
 
-    const csvRows = observations.map((observation) => ({
-        filename: observation.PK,
-        importance: observation.importance,
-        category: observation.category,
-        observation: observation.observation,
-        registrationNumber: observation.registrationNumber,
-        service: observation.service,
-        details: observation.details,
-    }));
+    let dynamoScanStartKey: Record<string, string> | undefined = undefined;
 
-    const csvHeaders = [
-        "filename",
-        "importance",
-        "category",
-        "observation",
-        "registrationNumber",
-        "service",
-        "details",
-    ];
+    do {
+        const dynamoScanOutput = await scanDynamo({
+            TableName: TNDS_OBSERVATION_TABLE_NAME,
+            ExclusiveStartKey: dynamoScanStartKey,
+        });
+        dynamoScanStartKey = dynamoScanOutput.LastEvaluatedKey;
 
-    let csvContent = `${csvHeaders.join(",")}\r\n`;
+        if (dynamoScanOutput.Items) {
+            for (const item of dynamoScanOutput.Items) {
+                try {
+                    const observation = observationSchema.parse(item);
+                    const dataSource = observation.dataSource;
+                    const file = observation.PK;
+                    const observationType = observation.observation;
 
-    if (csvRows.length) {
-        csvContent += `${csvRows.map((row) => Object.values(row).join(",")).join("\r\n")}\r\n`;
+                    if (!observationByDataSourceMap[dataSource]) {
+                        observationByDataSourceMap[dataSource] = {
+                            "Dataset Date": formattedDate,
+                            "Data Source": dataSource,
+                            "Total observations": 0,
+                            "Critical observations": 0,
+                            "Advisory observations": 0,
+                            "No timing point for more than 15 minutes": 0,
+                            "First stop is not a timing point": 0,
+                            "Last stop is not a timing point": 0,
+                            "Last stop is pick up only": 0,
+                            "First stop is set down only": 0,
+                            "Stop not found in NaPTAN": 0,
+                            "Incorrect stop type": 0,
+                            "Missing journey code": 0,
+                            "Duplicate journey code": 0,
+                            "Duplicate journey": 0,
+                            "Missing bus working number": 0,
+                            "Serviced organisation out of date": 0,
+                        };
+                    }
+
+                    observationByDataSourceMap[dataSource]["Total observations"]++;
+                    observationByDataSourceMap[dataSource][observationType]++;
+
+                    if (observation.importance === "critical") {
+                        observationByDataSourceMap[dataSource]["Critical observations"]++;
+                    } else {
+                        observationByDataSourceMap[dataSource]["Advisory observations"]++;
+                    }
+
+                    if (!observationByFileMap[file]) {
+                        observationByFileMap[file] = {
+                            "Dataset Date": formattedDate,
+                            Region: observation.region,
+                            File: file,
+                            "Data Source": dataSource,
+                            "Total observations": 0,
+                            "Critical observations": 0,
+                            "Advisory observations": 0,
+                            "No timing point for more than 15 minutes": 0,
+                            "First stop is not a timing point": 0,
+                            "Last stop is not a timing point": 0,
+                            "Last stop is pick up only": 0,
+                            "First stop is set down only": 0,
+                            "Stop not found in NaPTAN": 0,
+                            "Incorrect stop type": 0,
+                            "Missing journey code": 0,
+                            "Duplicate journey code": 0,
+                            "Duplicate journey": 0,
+                            "Missing bus working number": 0,
+                            "Serviced organisation out of date": 0,
+                        };
+                    }
+
+                    observationByFileMap[file]["Total observations"]++;
+                    observationByFileMap[file][observationType]++;
+
+                    if (observation.importance === "critical") {
+                        observationByFileMap[file]["Critical observations"]++;
+                    } else {
+                        observationByFileMap[file]["Advisory observations"]++;
+                    }
+
+                    if (!observationByObservationTypesMap[observationType][file]) {
+                        observationByObservationTypesMap[observationType][file] = {
+                            "Dataset Date": formattedDate,
+                            Region: observation.region,
+                            File: file,
+                            "National Operator Code": observation.noc,
+                            "Line Name": observation.registrationNumber,
+                            Quantity: 0,
+                        };
+                    }
+
+                    observationByObservationTypesMap[observationType][file].Quantity++;
+                } catch (error) {
+                    logger.error(error, "Error parsing dynamo item");
+                }
+            }
+        }
+    } while (dynamoScanStartKey);
+
+    const archive = archiver("zip", {});
+
+    try {
+        const passThrough = new PassThrough();
+        archive.pipe(passThrough);
+        const s3Upload = startS3Upload(TNDS_ANALYSIS_BUCKET_NAME, `${date}.zip`, passThrough, "application/zip");
+
+        const observationByDataSourceItemsCsv = createCsv(Object.values(observationByDataSourceMap));
+
+        if (observationByDataSourceItemsCsv) {
+            archive.append(observationByDataSourceItemsCsv, { name: "observationSummariesByDataSource.csv" });
+        }
+
+        const observationByFileItemsCsv = createCsv(Object.values(observationByFileMap));
+
+        if (observationByFileItemsCsv) {
+            archive.append(observationByFileItemsCsv, { name: "observationSummariesByFile.csv" });
+        }
+
+        for (const [observationType, observationByObservationTypeMap] of Object.entries(
+            observationByObservationTypesMap,
+        )) {
+            const observationByObservationTypeCsv = createCsv(Object.values(observationByObservationTypeMap));
+
+            if (observationByObservationTypeCsv) {
+                archive.append(observationByObservationTypeCsv, {
+                    name: `observationSummariesByObservationType/${observationType}.csv`,
+                });
+            }
+        }
+
+        await archive.finalize();
+        await s3Upload.done();
+    } catch (error) {
+        archive.abort();
+        logger.error(error, "Error creating and uploading zip file");
+        throw error;
     }
-
-    await putS3Object({
-        Bucket: TNDS_ANALYSIS_BUCKET_NAME,
-        Key: `${event.prefix}.csv`,
-        ContentType: "text/csv",
-        Body: csvContent,
-    });
 };
