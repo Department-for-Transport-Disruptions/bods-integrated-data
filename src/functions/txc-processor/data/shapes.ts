2ce081145e008ab2d79b3aa89da71937ef2c37c2
Laurence Jones <laurence.jones@doza.consulting>
2025-04-11 15:29:31 +0100

    fix: missing serviced org trips, out of order shapes (#560)

--- src/functions/txc-processor/data/shapes.ts
+++ src/functions/txc-processor/data/shapes.ts
@@ -48,7 +48,8 @@ export const getRouteLinks = (
 ): TxcRouteLink[] => {
     const route = routes.find((route) => route["@_id"] === routeRef);
 
-    const routeSectionsForRoute = routeSections.filter((section) => route?.RouteSectionRef.includes(section["@_id"]));
+    const routeSectionsForRoute =
+        route?.RouteSectionRef.map((ref) => routeSections.find((rs) => rs["@_id"] === ref)).filter(notEmpty) ?? [];
 
     if (!routeSectionsForRoute.length) {
         logger.warn(`Unable to find route sections for route: ${routeRef}`);
@@ -101,6 +102,7 @@ export const processShapes = async (
     vehicleJourneyMappings: VehicleJourneyMapping[],
 ) => {
     const { routeRefs, journeyPatternToRouteRefMapping } = getRouteRefs(routes, vehicleJourneyMappings);
+
     const updatedVehicleJourneyMappings = structuredClone(vehicleJourneyMappings);
 
     const shapes = routeRefs.flatMap<NewShape>((routeRef) => {
