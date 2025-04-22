4a7c60e94b497cbb0b10b2761178eb972c5c4d0d
Deanna Sharma <deanna.sharma@doza.consulting>
2025-04-22 16:02:00 +0100

    feat: update mock send data function test

--- src/functions/mock-data-producer-send-data/index.test.ts
+++ src/functions/mock-data-producer-send-data/index.test.ts
@@ -163,6 +163,16 @@ const expectedAvlXml = (subscriptionId: string) => `<?xml version="1.0" encoding
                     </VehicleJourney>
                 </Extensions>
             </VehicleActivity>
+            <VehicleActivityCancellation>
+                <RecordedAtTime>2018-08-17T15:22:20</RecordedAtTime>
+                <VehicleMonitoringRef>TEST</VehicleMonitoringRef>
+                <VehicleJourneyRef>
+                  <DataFrameRef>2018-08-17</DataFrameRef>
+                  <DatedVehicleJourneyRef>1234</DatedVehicleJourneyRef>
+                </VehicleJourneyRef>
+                <LineRef>567</LineRef>
+                <DirectionRef>inbound</DirectionRef>
+            </VehicleActivityCancellation>
         </VehicleMonitoringDelivery>
     </ServiceDelivery>
 </Siri>`;
