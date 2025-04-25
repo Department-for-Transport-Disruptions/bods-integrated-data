459fa5cf59a6b1e61da86c85624239809cec3c1c
Nathan Betts <nbetts@users.noreply.github.com>
2025-04-25 16:46:07 +0100

    fix: proper stop area null check before DB query (#573)
    Co-authored-by: Nathan Betts <13869955+nbetts@users.noreply.github.com>

--- src/functions/txc-processor/data/database.ts
+++ src/functions/txc-processor/data/database.ts
@@ -135,15 +135,11 @@ export const getNaptanStops = (dbClient: KyselyDb, atcoCodes: string[], useStopL
         .execute();
 };
 
-export const getNaptanStopAreas = (dbClient: KyselyDb, stopAreaCodes: (string | null)[]) => {
+export const getNaptanStopAreas = (dbClient: KyselyDb, stopAreaCodes: string[]) => {
     return dbClient
         .selectFrom("naptan_stop_area_new")
         .selectAll("naptan_stop_area_new")
-        .where(
-            "naptan_stop_area_new.stop_area_code",
-            "in",
-            stopAreaCodes.filter((code) => code !== null),
-        )
+        .where("naptan_stop_area_new.stop_area_code", "in", stopAreaCodes)
         .execute();
 };
 
