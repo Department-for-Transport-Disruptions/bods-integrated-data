ef367e26f7dbae44851c01147145f7816625d1ec
Laurence Jones <laurence.jones@doza.consulting>
2025-04-29 12:18:40 +0100

    fix: reduce number of requests to naptan api (#578)

--- src/functions/naptan-retriever/index.ts
+++ src/functions/naptan-retriever/index.ts
@@ -1,6 +1,7 @@
 import { KyselyDb, getDatabaseClient } from "@bods-integrated-data/shared/database";
 import { errorMapWithDataLogging, logger, withLambdaRequestTracker } from "@bods-integrated-data/shared/logger";
 import { putS3Object } from "@bods-integrated-data/shared/s3";
+import { chunkArray } from "@bods-integrated-data/shared/utils";
 import { Handler } from "aws-lambda";
 import axios from "axios";
 import { z } from "zod";
@@ -25,36 +26,45 @@ const retrieveNaptanCsvData = async (bucketName: string) => {
 const getAtcoAreaCodes = async (dbClient: KyselyDb) => {
     const nptgAdminAreas = await dbClient
         .selectFrom("nptg_admin_area_new")
-        .selectAll()
+        .select(["atco_code"])
         .distinctOn(["atco_code"])
+        .where("atco_code", "!=", "900") // Ignore National Coach area code since the naptan api thinks it's invalid
         .execute();
     return nptgAdminAreas.map((nptgAdminArea) => nptgAdminArea.atco_code);
 };
 
-const retrieveNaptanXmlData = async (bucketName: string, atcoAreaCodes: string[]) => {
-    for await (const atcoAreaCode of atcoAreaCodes) {
-        try {
-            const response = await axios.get(
-                `https://naptan.api.dft.gov.uk/v1/access-nodes?dataFormat=xml&atcoAreaCodes=${atcoAreaCode}`,
-                {
-                    responseType: "arraybuffer",
-                },
-            );
+const retrieveNaptanDataForAreaCodes = async (areaCodes: string[], fileName: string, bucketName: string) => {
+    const joinedAreaCodes = areaCodes.join(",");
+    logger.info(`Retrieving Naptan XML data for ATCO area code: ${joinedAreaCodes}`);
 
-            await putS3Object({
-                Bucket: bucketName,
-                Key: `${atcoAreaCode}.xml`,
-                ContentType: "application/xml",
-                Body: response.data as string,
-            });
-        } catch (e) {
-            if (axios.isAxiosError(e)) {
-                logger.error(e, `Error retrieving Naptan XML data for ATCO area code: ${atcoAreaCode}`);
-            } else {
-                logger.error(e, "Unknown error occurred");
-            }
+    try {
+        const response = await axios.get(
+            `https://naptan.api.dft.gov.uk/v1/access-nodes?dataFormat=xml&atcoAreaCodes=${joinedAreaCodes}`,
+            {
+                responseType: "arraybuffer",
+            },
+        );
+
+        await putS3Object({
+            Bucket: bucketName,
+            Key: `${fileName}.xml`,
+            ContentType: "application/xml",
+            Body: response.data as string,
+        });
+    } catch (e) {
+        if (axios.isAxiosError(e)) {
+            logger.error(e, `Error retrieving Naptan XML data for ATCO area codes: ${joinedAreaCodes}`);
+        } else {
+            logger.error(e, "Unknown error occurred");
         }
     }
+};
+
+const retrieveNaptanXmlData = async (bucketName: string, atcoAreaCodes: string[]) => {
+    const chunkedAreaCodes = chunkArray(atcoAreaCodes, Math.ceil(atcoAreaCodes.length / 2));
+
+    await retrieveNaptanDataForAreaCodes(chunkedAreaCodes[0], "AreaBatch1", bucketName);
+    await retrieveNaptanDataForAreaCodes(chunkedAreaCodes[1], "AreaBatch2", bucketName);
 };
 
 export const handler: Handler = async (event, context) => {
--- src/shared/schema/naptan.schema.ts
+++ src/shared/schema/naptan.schema.ts
@@ -1,5 +1,4 @@
 import { z } from "zod";
-import { NewNaptanStop, NewNaptanStopArea } from "../database";
 
 const selfClosingTag = z.literal("");
 const emptyTag = selfClosingTag.transform(() => undefined);
@@ -123,84 +122,6 @@ export const naptanSchema = z.object({
             })
             .optional(),
     }),
-});
-
-export const naptanSchemaTransformed = naptanSchema.transform((item) => {
-    const stopPoints: NewNaptanStop[] = [];
-    const stopAreas: NewNaptanStopArea[] = [];
-
-    if (item.NaPTAN.StopPoints.StopPoint.length > 0) {
-        const transformedStopPoints = item.NaPTAN.StopPoints.StopPoint.map((stop) => {
-            const newStop: NewNaptanStop = {
-                atco_code: stop.AtcoCode.toUpperCase(),
-                naptan_code: stop.NaptanCode ?? null,
-                plate_code: stop.PlateCode ?? null,
-                cleardown_code: stop.CleardownCode ?? null,
-                common_name: stop.Descriptor.CommonName ?? null,
-                short_common_name: stop.Descriptor.ShortCommonName ?? null,
-                landmark: stop.Descriptor.Landmark ?? null,
-                street: stop.Descriptor.Street ?? null,
-                crossing: stop.Descriptor.Crossing ?? null,
-                indicator: stop.Descriptor.Indicator ?? null,
-                bearing:
-                    stop.StopClassification.OnStreet?.Bus?.MarkedPoint?.Bearing.CompassPoint ??
-                    stop.StopClassification.OnStreet?.Bus?.UnmarkedPoint?.Bearing.CompassPoint ??
-                    null,
-                nptg_locality_code: stop.Place.NptgLocalityRef,
-                locality_name: stop.Place.LocalityName ?? null,
-                town: stop.Place.Town ?? null,
-                suburb: stop.Place.Suburb ?? null,
-                locality_centre: stop.Place.LocalityCentre ?? null,
-                grid_type: stop.Place.Location.Translation?.GridType ?? null,
-                easting: stop.Place.Location.Translation?.Easting ?? null,
-                northing: stop.Place.Location.Translation?.Northing ?? null,
-                longitude: stop.Place.Location.Translation?.Longitude ?? null,
-                latitude: stop.Place.Location.Translation?.Latitude ?? null,
-                stop_type: stop.StopClassification.StopType,
-                bus_stop_type: stop.StopClassification.OnStreet?.Bus?.BusStopType,
-                timing_status:
-                    stop.StopClassification.OnStreet?.Bus?.TimingStatus ??
-                    stop.StopClassification.OffStreet?.BusAndCoach?.Bay?.TimingStatus ??
-                    stop.StopClassification.OffStreet?.BusAndCoach?.VariableBay?.TimingStatus ??
-                    null,
-                default_wait_time: stop.StopClassification.OnStreet?.Bus?.MarkedPoint?.DefaultWaitTime ?? null,
-                notes: stop.StopFurtherDetails?.Notes ?? null,
-                administrative_area_code: stop.AdministrativeAreaRef,
-                creation_date_time: null,
-                modification_date_time: null,
-                revision_number: null,
-                modification: null,
-                status: null,
-                stop_area_code:
-                    stop.StopAreas?.StopAreaRef?.length === 1 ? stop.StopAreas.StopAreaRef[0].toUpperCase() : null,
-            };
-
-            return newStop;
-        });
-
-        stopPoints.push(...transformedStopPoints);
-    }
-
-    if (item.NaPTAN.StopAreas && item.NaPTAN.StopAreas.StopArea.length > 0) {
-        const transformedStopAreas = item.NaPTAN.StopAreas.StopArea.map((stopArea) => {
-            const newStopArea: NewNaptanStopArea = {
-                stop_area_code: stopArea.StopAreaCode.toUpperCase(),
-                name: stopArea.Name,
-                administrative_area_code: stopArea.AdministrativeAreaRef,
-                stop_area_type: stopArea.StopAreaType,
-                grid_type: stopArea.Location.Translation?.GridType ?? null,
-                easting: stopArea.Location.Translation?.Easting ?? null,
-                northing: stopArea.Location.Translation?.Northing ?? null,
-                longitude: stopArea.Location.Translation?.Longitude ?? null,
-                latitude: stopArea.Location.Translation?.Latitude ?? null,
-            };
-            return newStopArea;
-        });
-
-        stopAreas.push(...transformedStopAreas);
-    }
-
-    return { stopPoints, stopAreas };
 });
 
 export type Naptan = z.infer<typeof naptanSchema>;
