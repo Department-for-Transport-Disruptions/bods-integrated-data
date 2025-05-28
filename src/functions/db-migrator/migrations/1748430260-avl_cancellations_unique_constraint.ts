cae5c78d12b7ec07e73a777db7f42d40c54cfa0e
Deanna Sharma <83244002+DeannaSharma@users.noreply.github.com>
2025-05-28 16:33:40 +0100

    feat: updates to avl cancellations (#613)

---
+++ src/functions/db-migrator/migrations/1748430260-avl_cancellations_unique_constraint.ts
@@ -0,0 +1,45 @@
+import { Database } from "@bods-integrated-data/shared/database";
+import { Kysely } from "kysely";
+
+export async function up(db: Kysely<Database>): Promise<void> {
+    await db.schema
+        .alterTable("avl_cancellation")
+        .dropConstraint("uniq_avl_cancellation_data_frame_journey_line_direction_ref")
+        .execute();
+
+    await db.schema
+        .alterTable("avl_cancellation")
+        .addUniqueConstraint("uniq_avl_cancellation_data_frame_journey_line_direction_ref", [
+            "data_frame_ref",
+            "dated_vehicle_journey_ref",
+            "line_ref",
+            "direction_ref",
+            "vehicle_monitoring_ref",
+        ])
+        .execute();
+
+    await db.schema
+        .createIndex("idx_avl_join")
+        .on("avl")
+        .columns(["data_frame_ref", "dated_vehicle_journey_ref", "line_ref", "direction_ref", "vehicle_monitoring_ref"])
+        .execute();
+}
+
+export async function down(db: Kysely<Database>): Promise<void> {
+    await db.schema
+        .alterTable("avl_cancellation")
+        .dropConstraint("uniq_avl_cancellation_data_frame_journey_line_direction_ref")
+        .execute();
+
+    await db.schema
+        .alterTable("avl_cancellation")
+        .addUniqueConstraint("uniq_avl_cancellation_data_frame_journey_line_direction_ref", [
+            "data_frame_ref",
+            "dated_vehicle_journey_ref",
+            "line_ref",
+            "direction_ref",
+        ])
+        .execute();
+
+    await db.schema.dropIndex("idx_avl_join").on("avl").execute();
+}
