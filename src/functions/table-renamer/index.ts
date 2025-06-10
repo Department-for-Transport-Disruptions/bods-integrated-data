dc620e94f5fd9e5b2beff2ed4b02be7fa97d07f6
Laurence Jones <laurence.jones@doza.consulting>
2025-06-10 19:55:15 +0100

    fix: handle unzipping large files, limit renamer checks

--- src/functions/table-renamer/index.ts
+++ src/functions/table-renamer/index.ts
@@ -12,37 +12,24 @@ export interface TableKey {
     table: keyof Database;
     newTable: keyof Database;
     key: ReferenceExpression<Database, keyof Database>;
+    requiredPercentage?: number;
 }
 
 // Rename BODS related tables
 const databaseTables: TableKey[] = [
-    { table: "calendar", newTable: "calendar_new", key: "id" },
-    { table: "calendar_date", newTable: "calendar_date_new", key: "id" },
-    { table: "stop", newTable: "stop_new", key: "id" },
-    { table: "shape", newTable: "shape_new", key: "id" },
-    { table: "trip", newTable: "trip_new", key: "id" },
-    { table: "frequency", newTable: "frequency_new", key: "id" },
-    { table: "stop_time", newTable: "stop_time_new", key: "id" },
+    { table: "calendar", newTable: "calendar_new", key: "id", requiredPercentage: 70 },
+    { table: "calendar_date", newTable: "calendar_date_new", key: "id", requiredPercentage: 70 },
+    { table: "stop", newTable: "stop_new", key: "id", requiredPercentage: 70 },
+    { table: "shape", newTable: "shape_new", key: "id", requiredPercentage: 70 },
+    { table: "trip", newTable: "trip_new", key: "id", requiredPercentage: 70 },
+    { table: "frequency", newTable: "frequency_new", key: "id", requiredPercentage: 70 },
+    { table: "stop_time", newTable: "stop_time_new", key: "id", requiredPercentage: 70 },
     { table: "noc_operator", newTable: "noc_operator_new", key: "noc" },
     { table: "naptan_stop", newTable: "naptan_stop_new", key: "atco_code" },
     { table: "naptan_stop_area", newTable: "naptan_stop_area_new", key: "stop_area_code" },
     { table: "nptg_admin_area", newTable: "nptg_admin_area_new", key: "admin_area_code" },
     { table: "nptg_locality", newTable: "nptg_locality_new", key: "locality_code" },
     { table: "nptg_region", newTable: "nptg_region_new", key: "region_code" },
-    { table: "tfl_block", newTable: "tfl_block_new", key: "id" },
-    { table: "tfl_block_calendar_day", newTable: "tfl_block_calendar_day_new", key: "id" },
-    { table: "tfl_destination", newTable: "tfl_destination_new", key: "id" },
-    { table: "tfl_garage", newTable: "tfl_garage_new", key: "id" },
-    { table: "tfl_journey", newTable: "tfl_journey_new", key: "id" },
-    { table: "tfl_journey_drive_time", newTable: "tfl_journey_drive_time_new", key: "id" },
-    { table: "tfl_journey_wait_time", newTable: "tfl_journey_wait_time_new", key: "id" },
-    { table: "tfl_line", newTable: "tfl_line_new", key: "id" },
-    { table: "tfl_operator", newTable: "tfl_operator_new", key: "id" },
-    { table: "tfl_pattern", newTable: "tfl_pattern_new", key: "id" },
-    { table: "tfl_route_geometry", newTable: "tfl_route_geometry_new", key: "id" },
-    { table: "tfl_stop_in_pattern", newTable: "tfl_stop_in_pattern_new", key: "id" },
-    { table: "tfl_stop_point", newTable: "tfl_stop_point_new", key: "id" },
-    { table: "tfl_vehicle", newTable: "tfl_vehicle_new", key: "id" },
 ];
 
 export const checkTables = async (dbClient: KyselyDb, tables: TableKey[]) => {
@@ -67,9 +54,9 @@ export const checkTables = async (dbClient: KyselyDb, tables: TableKey[]) => {
 
         const percentageResult = (Number(newCount.count) / Number(currentCount.count)) * 100;
 
-        if (percentageResult < 80) {
+        if (t.requiredPercentage && percentageResult < t.requiredPercentage) {
             throw new Error(
-                `Tables ${table} and ${newTable} have less than an 80% match, percentage match: ${percentageResult}%`,
+                `Tables ${table} and ${newTable} have less than an ${t.requiredPercentage}% match, percentage match: ${percentageResult}%`,
             );
         }
 
