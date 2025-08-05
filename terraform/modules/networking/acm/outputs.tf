691bf305e8f31490364123bf0210de9d3f79eaca
Deanna Sharma <deanna.sharma@doza.consulting>
2025-08-05 13:22:43 +0100

    chore: remove unused acm certificate

--- terraform/modules/networking/acm/outputs.tf
+++ terraform/modules/networking/acm/outputs.tf
@@ -1,7 +1,3 @@
 output "acm_certificate_arn" {
   value = aws_acm_certificate.integrated_data_acm_certificate.arn
 }
-
-output "cloudfront_acm_certificate_arn" {
-  value = aws_acm_certificate.integrated_data_cloudfront_acm_certificate.arn
-}
