7a9bea9dbafc4d437c96c07b35e42c708920e4e9
Deanna Sharma <deanna.sharma@doza.consulting>
2025-08-08 10:24:34 +0100

    chore: lint and types

--- cli-helpers/src/commands/export-avl-validation-errors.ts
+++ cli-helpers/src/commands/export-avl-validation-errors.ts
@@ -1,12 +1,13 @@
 import { writeFileSync } from "node:fs";
-import { program } from "commander";
-import { mkConfig, generateCsv, asString } from "export-to-csv";
 import { getDate } from "@bods-integrated-data/shared/dates";
 import { logger } from "@bods-integrated-data/shared/logger";
+import { AvlValidationError, avlValidationErrorsSchema } from "@bods-integrated-data/shared/schema";
+import { program } from "commander";
+import { asString, generateCsv, mkConfig } from "export-to-csv";
+import { STAGES, recursiveScan, withUserPrompt } from "../utils";
 import { createDynamoDbDocClient } from "../utils/awsClients";
-import { recursiveScan, STAGES, withUserPrompt } from "../utils";
 
-const saveResultsToCsv = (data: any[], filename: string) => {
+const saveResultsToCsv = (data: AvlValidationError[], filename: string) => {
     if (data.length === 0) {
         logger.info("No data to save");
         return;
@@ -38,12 +39,14 @@ program
 
         try {
             logger.info(`Scanning DynamoDB table: ${tableName}`);
-            const data = await recursiveScan(dynamoDbClient, { TableName: tableName });
+            const tableItems = await recursiveScan(dynamoDbClient, { TableName: tableName });
+
+            const validationErrors = avlValidationErrorsSchema.safeParse(tableItems).data || [];
 
             const today = getDate().format("YYYY-MM-DD");
             const filename = `integrated-data-avl-validation-errors-${today}.csv`;
 
-            saveResultsToCsv(data, filename);
+            saveResultsToCsv(validationErrors, filename);
         } catch (error) {
             logger.error(error, "Error scanning DynamoDB table");
         } finally {
