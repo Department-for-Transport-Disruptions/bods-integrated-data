93b6ae04fb46ac0dfd9532ec8e2d9419e2bf3474
Marian Aden <marian.aden@kainos.com>
2026-05-22 12:21:22 +0100

    fix: add dev defaults for naptan terraform variables

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -1,6 +1,7 @@
 variable "naptan_bucket" {
   type        = string
   description = "Name of the external S3 bucket containing NaPTAN data"
+  default     = "bods-1297-data-landing-zone"
 }
 
 variable "naptan_arn" {
@@ -12,11 +13,13 @@ variable "naptan_arn" {
 variable "naptan_bucket_region" {
   type        = string
   description = "Region of the external S3 bucket containing NaPTAN data"
+  default     = "eu-west-2"
 }
 
 variable "naptan_xml_filename" {
   type        = string
   description = "S3 key/path of the NaPTAN XML file in the external bucket"
+  default     = "raw/naptan/naptan-latest_xml.xml"
 }
 
 
