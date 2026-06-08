abc3ba83442d41cd45e71837da492d525411992c
Caelainn Brinkley <caelainn.brinkley@kainos.com>
2026-06-08 14:27:53 +0100

    Updating the NOC-pipeline variables

--- terraform/modules/data-pipelines/noc-pipeline/variables.tf
+++ terraform/modules/data-pipelines/noc-pipeline/variables.tf
@@ -36,3 +36,27 @@ variable "db_name" {
   type    = string
   default = "bods_integrated_data"
 }
+
+variable "noc_bucket_name" {
+  type    = string
+  description = "Name of the external S3 bucket containing NPTG data"
+  default     = ""
+}
+
+variable "noc__arn" {
+  type        = string
+  description = "ARN of the IAM role to assume for cross-account S3 access"
+  default     = ""
+}
+
+variable "bucket_region" {
+  type        = string
+  description = "Region of the external S3 bucket containing NOC data"
+  default     = ""
+}
+
+variable "noc_s3_key" {
+  type        = string
+  description = "S3 key/path of the NPTG XML/CSV file in the external bucket"
+  default     = ""
+}
