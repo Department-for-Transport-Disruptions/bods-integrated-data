80b64f35537aeb266e731d9d88fcdf5199613d22
Rob Whittle <69803842+rob-whittle@users.noreply.github.com>
2026-06-17 14:33:37 +0100

    match existing secret name style

--- terraform/modules/database/aurora-db/main.tf
+++ terraform/modules/database/aurora-db/main.tf
@@ -284,6 +284,6 @@ resource "aws_route53_record" "integrated_data_db_reader_cname_record" {
 }
 
 resource "aws_secretsmanager_secret" "integrated_data_db_user_ro" {
-  name = "integrated-data-${var.environment}-db-user-ro"
+  name = "database_read_only_user"
   description = "Secret for the integrated data database read-only user"
 }
