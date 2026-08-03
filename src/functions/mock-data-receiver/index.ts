d76cee4106d17676439b21a87528968cfaffafde
Laurence Jones <laurence.jones@kainos.com>
2026-08-03 10:11:57 +0100

    fix: remove expensive log line

--- src/functions/mock-data-receiver/index.ts
+++ src/functions/mock-data-receiver/index.ts
@@ -1,4 +1,4 @@
-import { errorMapWithDataLogging, logger, withLambdaRequestTracker } from "@bods-integrated-data/shared/logger";
+import { errorMapWithDataLogging, withLambdaRequestTracker } from "@bods-integrated-data/shared/logger";
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { z } from "zod";
 
@@ -6,8 +6,6 @@ z.setErrorMap(errorMapWithDataLogging);
 
 export const handler: APIGatewayProxyHandler = async (event, context) => {
     withLambdaRequestTracker(event ?? {}, context ?? {});
-
-    logger.info(event.body);
 
     return {
         statusCode: Number(event.queryStringParameters?.statusCode) || 200,
